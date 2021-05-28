363ddf19cdcea4a12799bbc3465fa459a39f17c7
graphicsforge <graphicsforge@gmail.com>
2021-05-27 21:35:40 -0400

    export components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 const Button = require("./components/Button").default;
-const PlainButton = require("./components/PlainButton").default;
+const ButtonGroup = require("./components/ButtonGroup").default;
+const Header = require("./components/Header").default;
 const Input = require("./components/Input").default;
+const Page = require("./components/Page").default;
+const PlainButton = require("./components/PlainButton").default;
+const Popover = require("./components/Popover").default;
 const GlobalStyles = require("./globalStyles").default;
 
-const pkg = { Button, PlainButton, Input, GlobalStyles };
-module.exports = pkg;
+module.exports = {
+  Button,
+  ButtonGroup,
+  Header,
+  Input,
+  Page,
+  PlainButton,
+  Popover,
+  GlobalStyles,
+};

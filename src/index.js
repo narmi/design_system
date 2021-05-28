801bf7603350804b3dfd0b2a9fd340228c45db5d
Frank Kotsianas <frank@narmitech.com>
2021-05-27 22:43:39 -0400

    remove Page and Header (leftover dummy components)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const Button = require("./components/Button").default;
 const ButtonGroup = require("./components/ButtonGroup").default;
-const Header = require("./components/Header").default;
 const Input = require("./components/Input").default;
-const Page = require("./components/Page").default;
 const PlainButton = require("./components/PlainButton").default;
 const Popover = require("./components/Popover").default;
 const GlobalStyles = require("./globalStyles").default;
@@ -10,9 +8,7 @@ const GlobalStyles = require("./globalStyles").default;
 module.exports = {
   Button,
   ButtonGroup,
-  Header,
   Input,
-  Page,
   PlainButton,
   Popover,
   GlobalStyles,

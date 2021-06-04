f65eb7b1c80ebc29824322e397ac4bd4ebc846c9
reshav-abraham <reshav@narmitech.com>
2021-06-04 17:13:46 -0400

    design system sidebar component

--- src/components/Sidebar/Sidebar.stories.js
+++ src/components/Sidebar/Sidebar.stories.js
@@ -2,7 +2,6 @@ import React from "react";
 import Sidebar from "components/Sidebar";
 import Typography from "components/Typography";
 import { Menu } from "react-feather";
-import styled, { css } from "styled-components";
 
 export default {
   title: "Components/Sidebar",
@@ -10,17 +9,6 @@ export default {
 };
 
 const Template = (args) => <Sidebar {...args} />;
-
-const StyledWrapper = styled.span`
-  // position: relative + position: absolute allows absolutely positioning child relative to parent
-  position: relative;
-  box-shadow: var(--nds-dropshadow-dark);
-  box-sizing: border-box;
-  width: 300px;
-  height: 100%;
-  bottom: 0;
-  font-family: var(--nds-font-family);
-`;
 
 export const SidebarMenu = Template.bind({});
 SidebarMenu.args = {

e7ced6521282470cf2815e067772764da8260807
reshav-abraham <reshav@narmitech.com>
2021-06-10 11:45:29 -0400

    navbar keys

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -97,13 +97,13 @@ const NavBar = (props) => {
     <StyledHeader {...props}>
       <StyledNavLeft logo={props.logo}>
         {props.logo ? <div>{props.logo}</div> : null}
-        {props.leftChildren.map((option) => (
-          <StyledMenuItem>{option}</StyledMenuItem>
+        {props.leftChildren.map((option, idx) => (
+          <StyledMenuItem key={idx}>{option}</StyledMenuItem>
         ))}
       </StyledNavLeft>
       <StyledNavRight>
-        {props.rightChildren.map((option) => (
-          <StyledMenuItem>{option}</StyledMenuItem>
+        {props.rightChildren.map((option, idx) => (
+          <StyledMenuItem key={idx}>{option}</StyledMenuItem>
         ))}
       </StyledNavRight>
       <StyledMobileMenuContainer {...props}>

e697d818b4ad4ce582a80aa231482a40d8cce8f8
reshav-abraham <reshav@narmitech.com>
2021-06-17 15:20:27 -0400

    styling

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -47,11 +47,6 @@ const CloseIcon = styled.span`
   padding-top: 15px;
 `;
 
-const SideBarItems = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
 const Sidebar = (props) => {
   const [open, setOpen] = React.useState(false);
   const openSidebar = () => {
@@ -69,7 +64,7 @@ const Sidebar = (props) => {
         <CloseIcon slideFromRight={props.slideFromRight}>
           <X onClick={closeSidebar} />
         </CloseIcon>
-        <SideBarItems>{props.menuItems}</SideBarItems>
+        {props.menuItems}
       </StyledSideBar>
     </>
   );

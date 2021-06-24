243ed238c60f3bd42b4402621ae6e3bc5654ebbd
reshav-abraham <reshav@narmitech.com>
2021-06-24 14:49:03 -0400

    typography

--- src/components/Typography/Typography.stories.js
+++ src/components/Typography/Typography.stories.js
@@ -22,59 +22,88 @@ TypographySpan.args = {
 export const Header1 = Template.bind({});
 Header1.args = {
   ...DefaultArgs,
+  H1: true,
   children: ["test"],
 };
 
 export const Header2 = Template.bind({});
 Header2.args = {
   ...DefaultArgs,
+  H2: true,
   children: ["test"],
 };
 
 export const Header3 = Template.bind({});
 Header3.args = {
   ...DefaultArgs,
+  H3: true,
   children: ["test"],
 };
 
-export const SubHeader = Template.bind({});
-SubHeader.args = {
+export const Subheader = Template.bind({});
+Subheader.args = {
   ...DefaultArgs,
+  Subheader: true,
   children: ["test"],
 };
 
 export const P1Regular = Template.bind({});
 P1Regular.args = {
   ...DefaultArgs,
+  P1: true,
   children: ["test"],
 };
 
 export const P1SemiBold = Template.bind({});
 P1SemiBold.args = {
   ...DefaultArgs,
+  P1: true,
+  SemiBold: true,
   children: ["test"],
 };
 
 export const P2Regular = Template.bind({});
 P2Regular.args = {
   ...DefaultArgs,
+  P2: true,
   children: ["test"],
 };
 
 export const P2SemiBold = Template.bind({});
 P2SemiBold.args = {
   ...DefaultArgs,
+  P2: true,
+  SemiBold: true,
   children: ["test"],
 };
 
 export const P3Regular = Template.bind({});
 P3Regular.args = {
   ...DefaultArgs,
+  P3: true,
   children: ["test"],
 };
 
 export const P3SemiBold = Template.bind({});
 P3SemiBold.args = {
   ...DefaultArgs,
+  P3: true,
+  SemiBold: true,
+  children: ["test"],
+};
+
+export const Link = Template.bind({});
+Link.args = {
+  ...DefaultArgs,
+  P1: true,
+  Link: true,
+  children: ["test"],
+};
+
+export const SmallLink = Template.bind({});
+SmallLink.args = {
+  ...DefaultArgs,
+  P1: true,
+  SmallLink: true,
   children: ["test"],
 };
--- src/components/Typography/index.js
+++ src/components/Typography/index.js
@@ -3,18 +3,89 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const StyledContainer = styled.span`
-  color: #4c4c4c;
-  font-weight: 400;
+  color: ${(props) => props.color};
+  font-weight: ${(props) => (props.SemiBold ? "600" : props.fontWeight)};
   font-family: var(--nds-font-family);
-  line-height: 20px;
+  line-height: ${(props) => props.lineHeight};
+  font-size: ${(props) => props.fontSize};
+  text-transform: ${(props) => (props.Subheader ? "uppercase" : null)};
+
+  :hover {
+    text-decoration: ${(props) =>
+      props.Link || props.SmallLink ? "underline" : null};
+    text-decoration-color: ${(props) =>
+      props.Link || props.SmallLink ? props.color : null};
+  }
 `;
 
 const Typography = (props) => {
-  return <StyledContainer {...props}> {props.children} </StyledContainer>;
+  let fontWeight = "400";
+  let fontSize = "16px";
+  let lineHeight = "20px";
+  let color = "#4c4c4c";
+  if (props.H1) {
+    fontWeight = "600";
+    lineHeight = "50px";
+    fontSize = "36px";
+  } else if (props.H2) {
+    fontWeight = "600";
+    lineHeight = "35px";
+    fontSize = "28px";
+  } else if (props.H3) {
+    fontWeight = "600";
+    lineHeight = "28px";
+    fontSize = "20px";
+  } else if (props.Subheader) {
+    fontWeight = "600";
+    lineHeight = "15px";
+    fontSize = "12px";
+  } else if (props.P3) {
+    lineHeight = "15px";
+    fontSize = "12px";
+  } else if (props.P2) {
+    lineHeight = "18px";
+    fontSize = "14px";
+  } else if (props.P1) {
+    lineHeight = "20px";
+    fontSize = "16px";
+  }
+  if (props.Link) {
+    fontSize = "20px";
+    fontWeight = "600";
+    color = "var(--nds-secondary-color)";
+  } else if (props.SmallLink) {
+    fontSize = "16px";
+    fontWeight = "600";
+    color = "var(--nds-secondary-color)";
+  }
+  return (
+    <StyledContainer
+      color={color}
+      fontWeight={fontWeight}
+      fontSize={fontSize}
+      lineHeight={lineHeight}
+      {...props}
+    >
+      {" "}
+      {props.children}{" "}
+    </StyledContainer>
+  );
 };
 
 Typography.propTypes = {
+  H1: PropTypes.bool,
+  H2: PropTypes.bool,
+  H3: PropTypes.bool,
+  Subheader: PropTypes.bool,
+
   P1: PropTypes.bool,
+  P2: PropTypes.bool,
+  P3: PropTypes.bool,
+
+  SemiBold: PropTypes.bool,
+
+  Link: PropTypes.bool,
+  SmallLink: PropTypes.bool,
   children: PropTypes.node,
 };
 

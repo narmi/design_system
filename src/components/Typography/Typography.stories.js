8d8990ebfd23d34fc5266695a6c0de9b2adc4b42
reshav-abraham <reshav@narmitech.com>
2021-06-24 10:26:59 -0400

    p1 type

--- src/components/Typography/Typography.stories.js
+++ src/components/Typography/Typography.stories.js
@@ -18,3 +18,63 @@ TypographySpan.args = {
   ...DefaultArgs,
   children: ["test"],
 };
+
+export const Header1 = Template.bind({});
+Header1.args = {
+  ...DefaultArgs,
+  children: ["test"],
+};
+
+export const Header2 = Template.bind({});
+Header2.args = {
+  ...DefaultArgs,
+  children: ["test"],
+};
+
+export const Header3 = Template.bind({});
+Header3.args = {
+  ...DefaultArgs,
+  children: ["test"],
+};
+
+export const SubHeader = Template.bind({});
+SubHeader.args = {
+  ...DefaultArgs,
+  children: ["test"],
+};
+
+export const P1Regular = Template.bind({});
+P1Regular.args = {
+  ...DefaultArgs,
+  children: ["test"],
+};
+
+export const P1SemiBold = Template.bind({});
+P1SemiBold.args = {
+  ...DefaultArgs,
+  children: ["test"],
+};
+
+export const P2Regular = Template.bind({});
+P2Regular.args = {
+  ...DefaultArgs,
+  children: ["test"],
+};
+
+export const P2SemiBold = Template.bind({});
+P2SemiBold.args = {
+  ...DefaultArgs,
+  children: ["test"],
+};
+
+export const P3Regular = Template.bind({});
+P3Regular.args = {
+  ...DefaultArgs,
+  children: ["test"],
+};
+
+export const P3SemiBold = Template.bind({});
+P3SemiBold.args = {
+  ...DefaultArgs,
+  children: ["test"],
+};
--- src/components/Typography/index.js
+++ src/components/Typography/index.js
@@ -6,6 +6,7 @@ const StyledContainer = styled.span`
   color: #4c4c4c;
   font-weight: 400;
   font-family: var(--nds-font-family);
+  line-height: 20px;
 `;
 
 const Typography = (props) => {
@@ -13,10 +14,12 @@ const Typography = (props) => {
 };
 
 Typography.propTypes = {
+  P1: PropTypes.bool,
   children: PropTypes.node,
 };
 
 Typography.defaultProps = {
+  P1: true,
   children: [],
 };
 

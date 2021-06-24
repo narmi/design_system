4f49a5a9979dc75a2f429194703ac839c9040ae9
Reshav Abraham <23458204+reshav-abraham@users.noreply.github.com>
2021-06-24 15:55:21 -0400

    [Ds/typography] L: Typography config (#64)
    Typography Types

--- src/components/Typography/Typography.stories.js
+++ src/components/Typography/Typography.stories.js
@@ -8,13 +8,88 @@ export default {
   decorators: [Centered],
 };
 
+const allLetters = "The wizard quickly jinxed the gnomes before they vaporized";
+
 const Template = (args) => <Typography {...args} />;
 const DefaultArgs = {
-  children: ["test"],
+  children: [allLetters],
 };
 
 export const TypographySpan = Template.bind({});
 TypographySpan.args = {
   ...DefaultArgs,
-  children: ["test"],
+  children: [allLetters],
+};
+
+export const Header1 = Template.bind({});
+Header1.args = {
+  ...DefaultArgs,
+  h1: true,
+  children: [allLetters],
+};
+
+export const Header2 = Template.bind({});
+Header2.args = {
+  ...DefaultArgs,
+  h2: true,
+  children: [allLetters],
+};
+
+export const Header3 = Template.bind({});
+Header3.args = {
+  ...DefaultArgs,
+  h3: true,
+  children: [allLetters],
+};
+
+export const Subheader = Template.bind({});
+Subheader.args = {
+  ...DefaultArgs,
+  subheader: true,
+  children: [allLetters],
+};
+
+export const P1Regular = Template.bind({});
+P1Regular.args = {
+  ...DefaultArgs,
+  p1: true,
+  children: [allLetters],
+};
+
+export const P1semibold = Template.bind({});
+P1semibold.args = {
+  ...DefaultArgs,
+  p1: true,
+  semibold: true,
+  children: [allLetters],
+};
+
+export const P2Regular = Template.bind({});
+P2Regular.args = {
+  ...DefaultArgs,
+  p2: true,
+  children: [allLetters],
+};
+
+export const P2semibold = Template.bind({});
+P2semibold.args = {
+  ...DefaultArgs,
+  p2: true,
+  semibold: true,
+  children: [allLetters],
+};
+
+export const P3Regular = Template.bind({});
+P3Regular.args = {
+  ...DefaultArgs,
+  p3: true,
+  children: [allLetters],
+};
+
+export const P3semibold = Template.bind({});
+P3semibold.args = {
+  ...DefaultArgs,
+  p3: true,
+  semibold: true,
+  children: [allLetters],
 };
--- src/components/Typography/index.js
+++ src/components/Typography/index.js
@@ -3,20 +3,74 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const StyledContainer = styled.span`
-  color: #4c4c4c;
-  font-weight: 400;
+  color: ${(props) => props.color};
+  font-weight: ${(props) => (props.semibold ? "600" : props.fontWeight)};
   font-family: var(--nds-font-family);
+  line-height: ${(props) => props.lineHeight};
+  font-size: ${(props) => props.fontSize};
+  text-transform: ${(props) => (props.subheader ? "uppercase" : null)};
 `;
 
 const Typography = (props) => {
-  return <StyledContainer {...props}> {props.children} </StyledContainer>;
+  let fontWeight = "400";
+  let fontSize = "16px";
+  let lineHeight = "20px";
+  let color = "var(--nds-grey-text)";
+  if (props.h1) {
+    fontWeight = "600";
+    lineHeight = "50px";
+    fontSize = "36px";
+  } else if (props.h2) {
+    fontWeight = "600";
+    lineHeight = "35px";
+    fontSize = "28px";
+  } else if (props.h3) {
+    fontWeight = "600";
+    lineHeight = "28px";
+    fontSize = "20px";
+  } else if (props.subheader) {
+    fontWeight = "600";
+    lineHeight = "15px";
+    fontSize = "12px";
+  } else if (props.p3) {
+    lineHeight = "15px";
+    fontSize = "12px";
+  } else if (props.p2) {
+    lineHeight = "18px";
+    fontSize = "14px";
+  } else if (props.p1) {
+    lineHeight = "20px";
+    fontSize = "16px";
+  }
+  return (
+    <StyledContainer
+      color={color}
+      fontWeight={fontWeight}
+      fontSize={fontSize}
+      lineHeight={lineHeight}
+      {...props}
+    >
+      {props.children}
+    </StyledContainer>
+  );
 };
 
 Typography.propTypes = {
+  h1: PropTypes.bool,
+  h2: PropTypes.bool,
+  h3: PropTypes.bool,
+  subheader: PropTypes.bool,
+
+  p1: PropTypes.bool,
+  p2: PropTypes.bool,
+  p3: PropTypes.bool,
+
+  semibold: PropTypes.bool,
   children: PropTypes.node,
 };
 
 Typography.defaultProps = {
+  p1: true,
   children: [],
 };
 

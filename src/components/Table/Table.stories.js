b1d7f9b3c5c2fc217947336dbbd2ecc38698a127
reshav-abraham <reshav@narmitech.com>
2021-06-29 11:33:50 -0400

    helper

--- src/components/Table/Table.stories.js
+++ src/components/Table/Table.stories.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Table from "components/Table";
 import Typography from "components/Typography";
+import { transfersGridData } from "../../helpers/sampleData.js"
 import PlainButton from "components/PlainButton";
 
 export default {
@@ -25,104 +26,7 @@ const Template = (args) => <Table {...args} />;
 const DefaultArgs = {
   title: "Scheduled transactions",
   hoverable: true,
-  gridData: [
-    [
-      {
-        column: "DESCRIPTION",
-        content: (
-          <DescriptionDiv
-            transfer={"Transfer to Noble Bank-3432"}
-            date={"October 30, 2020"}
-          />
-        ),
-        sortKey: "Transfer to Noble Bank-3432",
-      },
-      {
-        column: "AMOUNT",
-        content: "-$1000",
-        sortKey: -1000,
-      },
-      {
-        content: (
-          <PlainButton style={{ textAlign: "right", fontSize: "14px" }} small>
-            Delete
-          </PlainButton>
-        ),
-      },
-    ],
-    [
-      {
-        column: "DESCRIPTION",
-        content: (
-          <DescriptionDiv
-            transfer={"Bill to Waterworks-4534"}
-            date={"October 23, 2020"}
-          />
-        ),
-        sortKey: "Bill to Waterworks-4534",
-      },
-      {
-        column: "AMOUNT",
-        content: "-$123.45",
-        sortKey: -123.45,
-      },
-      {
-        content: (
-          <PlainButton style={{ textAlign: "right", fontSize: "14px" }} small>
-            Delete
-          </PlainButton>
-        ),
-      },
-    ],
-    [
-      {
-        column: "DESCRIPTION",
-        content: (
-          <DescriptionDiv
-            transfer={"Transfer to Melanie Abrazado"}
-            date={"October 20, 2020"}
-          />
-        ),
-        sortKey: "Transfer to Melanie Abrazado",
-      },
-      {
-        column: "AMOUNT",
-        content: "-$80",
-        sortKey: -80,
-      },
-      {
-        content: (
-          <PlainButton style={{ textAlign: "right", fontSize: "14px" }} small>
-            Delete
-          </PlainButton>
-        ),
-      },
-    ],
-    [
-      {
-        column: "DESCRIPTION",
-        content: (
-          <DescriptionDiv
-            transfer={"Transfer to Melanie Abrazado"}
-            date={"October 21, 2020"}
-          />
-        ),
-        sortKey: "Transfer to Melanie Abrazado",
-      },
-      {
-        column: "AMOUNT",
-        content: "-$130",
-        sortKey: -130,
-      },
-      {
-        content: (
-          <PlainButton style={{ textAlign: "right", fontSize: "14px" }} small>
-            Delete
-          </PlainButton>
-        ),
-      },
-    ],
-  ],
+  gridData: transfersGridData,
 };
 
 export const BasicTable = Template.bind({});
---
+++ src/helpers/sampleData.js
@@ -0,0 +1,255 @@
+import React from "react";
+import Typography from "components/Typography";
+import PlainButton from "components/PlainButton";
+
+const DescriptionDiv = (props) => {
+    return (
+      <div style={{ display: "flex", flexDirection: "column", width: "220px" }}>
+        <Typography style={{ fontSize: "16px" }}>{props.transfer}</Typography>
+        <Typography
+          style={{ fontSize: "14px", color: "var(--nds-grey-placeholder)" }}
+        >
+          {props.date}
+        </Typography>
+      </div>
+    );
+  };
+
+  
+export const transfersGridData = [
+    [
+      {
+        column: "DESCRIPTION",
+        content: (
+          <DescriptionDiv
+            transfer={"Transfer to Noble Bank-3432"}
+            date={"October 30, 2020"}
+          />
+        ),
+        sortKey: "Transfer to Noble Bank-3432",
+      },
+      {
+        column: "AMOUNT",
+        content: "-$1000",
+        sortKey: -1000,
+      },
+      {
+        content: (
+          <PlainButton style={{ textAlign: "right", fontSize: "14px" }} small>
+            Delete
+          </PlainButton>
+        ),
+      },
+    ],
+    [
+      {
+        column: "DESCRIPTION",
+        content: (
+          <DescriptionDiv
+            transfer={"Bill to Waterworks-4534"}
+            date={"October 23, 2020"}
+          />
+        ),
+        sortKey: "Bill to Waterworks-4534",
+      },
+      {
+        column: "AMOUNT",
+        content: "-$123.45",
+        sortKey: -123.45,
+      },
+      {
+        content: (
+          <PlainButton style={{ textAlign: "right", fontSize: "14px" }} small>
+            Delete
+          </PlainButton>
+        ),
+      },
+    ],
+    [
+      {
+        column: "DESCRIPTION",
+        content: (
+          <DescriptionDiv
+            transfer={"Transfer to Melanie Abrazado"}
+            date={"October 20, 2020"}
+          />
+        ),
+        sortKey: "Transfer to Melanie Abrazado",
+      },
+      {
+        column: "AMOUNT",
+        content: "-$80",
+        sortKey: -80,
+      },
+      {
+        content: (
+          <PlainButton style={{ textAlign: "right", fontSize: "14px" }} small>
+            Delete
+          </PlainButton>
+        ),
+      },
+    ],
+    [
+      {
+        column: "DESCRIPTION",
+        content: (
+          <DescriptionDiv
+            transfer={"Transfer to Melanie Abrazado"}
+            date={"October 21, 2020"}
+          />
+        ),
+        sortKey: "Transfer to Melanie Abrazado",
+      },
+      {
+        column: "AMOUNT",
+        content: "-$130",
+        sortKey: -130,
+      },
+      {
+        content: (
+          <PlainButton style={{ textAlign: "right", fontSize: "14px" }} small>
+            Delete
+          </PlainButton>
+        ),
+      },
+    ],
+  ];
+
+
+  export const adminGridData = [
+    [
+      {
+        column: "name",
+        content: <Typography>Maria Borges</Typography>,
+        sortKey: "Maria Borges",
+      },
+      {
+        column: "email",
+        content: <Typography>mariajoseluisborges@narmitech.com</Typography>,
+        sortKey: "mariajoseluisborges@narmitech.com",
+      },
+      {
+        column: "role",
+        content: <Typography>Collaborator</Typography>,
+        sortKey: "Collaborator",
+      },
+      {
+        column: "status",
+        content: <Typography>Active</Typography>,
+        sortKey: "Active",
+      },
+      {
+        column: "Last Login",
+        content: <Typography>07/01/20</Typography>,
+        sortKey: "07/04/20",
+      },
+    ],
+    [
+        {
+            column: "name",
+            content: <Typography>Angela Davis</Typography>,
+            sortKey: "Angela Davis",
+          },
+          {
+            column: "email",
+            content: <Typography>angeladavis@narmitech.com</Typography>,
+            sortKey: "angeladavis@narmitech.com",
+          },
+          {
+            column: "role",
+            content: <Typography>Admin</Typography>,
+            sortKey: "Admin",
+          },
+          {
+            column: "status",
+            content: <Typography>Active</Typography>,
+            sortKey: "Active",
+          },
+          {
+            column: "Last Login",
+            content: <Typography>07/04/20</Typography>,
+            sortKey: "07/04/20",
+          },
+    ],
+    [
+        {
+            column: "name",
+            content: <Typography>Junot Diaz</Typography>,
+            sortKey: "Junot Diaz",
+          },
+          {
+            column: "email",
+            content: <Typography>junotdiaz@narmitech.com</Typography>,
+            sortKey: "junotdiaz@narmitech.com",
+          },
+          {
+            column: "role",
+            content: <Typography>Viewer</Typography>,
+            sortKey: "Viewer",
+          },
+          {
+            column: "status",
+            content: <Typography>Active</Typography>,
+            sortKey: "Active",
+          },
+          {
+            column: "Last Login",
+            content: <Typography>07/12/20</Typography>,
+            sortKey: "07/12/20",
+          },
+    ],
+    [
+        {
+            column: "name",
+            content: <Typography>Aminatou Sow</Typography>,
+            sortKey: "Aminatou Sow",
+          },
+          {
+            column: "email",
+            content: <Typography>aminatousow@narmitech.com</Typography>,
+            sortKey: "junotdiaz@narmitech.com",
+          },
+          {
+            column: "role",
+            content: <Typography>Collaborator</Typography>,
+            sortKey: "Collaborator",
+          },
+          {
+            column: "status",
+            content: <Typography>Active</Typography>,
+            sortKey: "Active",
+          },
+          {
+            column: "Last Login",
+            content: <Typography>10/24/19</Typography>,
+            sortKey: "10/24/19",
+          },
+    ],
+    [
+        {
+            column: "name",
+            content: <Typography>Zadie Smith</Typography>,
+            sortKey: "Zadie Smith",
+          },
+          {
+            column: "email",
+            content: <Typography>zadiesmith@narmitech.com</Typography>,
+            sortKey: "zadiesmith@narmitech.com",
+          },
+          {
+            column: "role",
+            content: <Typography>Collaborator</Typography>,
+            sortKey: "Collaborator",
+          },
+          {
+            column: "status",
+            content: <Typography>Active</Typography>,
+            sortKey: "Active",
+          },
+          {
+            column: "Last Login",
+            content: <Typography>04/29/20</Typography>,
+            sortKey: "04/29/20",
+          },
+    ]
+  ];

9cd04a0d1ad0138932f33ebc910b564c2f005ee0
Frank Kotsianas <frank@narmitech.com>
2021-06-29 16:00:51 -0400

    Card desktop width in 372px

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,14 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Typography from "components/Typography";
+import {deviceBreakpoints} from "../../globalStyles";
 
 const StyledCard = styled.div`
   box-shadow: var(--nds-dropshadow-dark);
   border-radius: 4px;
   padding: 20px;
   margin-right 20px;
+  width: 100%;
+  max-width: 372px;
+  min-height: 68px;
+  box-sizing: border-box;
+
   :last-child {
     margin-right: 0px;
+  }
+
+  @media (max-width: ${deviceBreakpoints.mobileMax}) {
+    max-width: auto;
   }
 `;
 

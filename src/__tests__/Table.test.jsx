98293f356ba71f40a0e82d8137b3b9644c548604
reshav-abraham <reshav@narmitech.com>
2021-07-07 13:29:57 -0400

    unit tests

--- src/__tests__/Table.test.jsx
+++ src/__tests__/Table.test.jsx
@@ -90,12 +90,9 @@ const sampleGridData = [
   ],
 ];
 
-describe("Table Sorting", () => {
-  it("Table should be renered initially by user specified order, \
-      then by ascending order by the Amount column, \
-      then in descending order by the Amount column", () => {
-        
-    const { queryAllByTestId, queryByTestId } = render(
+describe("Table Sorting Amount Tests", () => {  
+  it("Table should be renered initially by user specified order", () => {
+    const { queryAllByTestId } = render(
       <Table
         title={"test"}
         gridData={sampleGridData}
@@ -107,31 +104,51 @@ describe("Table Sorting", () => {
     let selectedColumn = queryAllByTestId("col1");
     let columnValues = selectedColumn.map((cell) => cell.textContent);
     expect(columnValues).toEqual(["-$1000", "-$123.45", "-$80", "-$130"]);
+  });
+
+  it("sorts by Amount in ascending order", () => {
+    const { queryByTestId, queryAllByTestId } = render(
+      <Table
+        title={"test"}
+        gridData={sampleGridData}
+        sortableHeaders={["DESCRIPTION", "AMOUNT"]}
+      />
+    );
+
+    let sortOption = queryByTestId("AMOUNT_ASC");
+    let selectedColumn = queryAllByTestId("col1");
+    sortOption.click();
+    let columnValues = selectedColumn.map((cell) => cell.textContent);
 
     // clicking ascending option for Amount should sort the table in ascending 
     // order by Amount
-    let sortOption = queryByTestId("AMOUNT_ASC");
-    sortOption.click();
-    columnValues = selectedColumn.map((cell) => cell.textContent);
     expect(columnValues).toEqual(["-$80", "-$123.45", "-$130", "-$1000"]);
+  });
+
+  it("sorts by Amount in descending order", () => {
+    const { queryByTestId, queryAllByTestId } = render(
+      <Table
+        title={"test"}
+        gridData={sampleGridData}
+        sortableHeaders={["DESCRIPTION", "AMOUNT"]}
+      />
+    );
+
+    let sortOption = queryByTestId("AMOUNT_DESC");
+    let selectedColumn = queryAllByTestId("col1");
+    sortOption.click();
+    let columnValues = selectedColumn.map((cell) => cell.textContent);
 
     // clicking descending option for Amount should sort the table in descending 
     // order by Amount
-    sortOption = queryByTestId("AMOUNT_DESC");
-    sortOption.click();
-    columnValues = selectedColumn.map((cell) => cell.textContent);
     expect(columnValues).toEqual(["-$1000", "-$130", "-$123.45", "-$80"]);
   });
 });
 
-describe("Active descending sort followed by reset", () => {
-  it("Table renders in user specified order, \
-      then ordered by Description in alphebetical order, \
-      then reset to the user specified order, \
-      then ordered by Description in reverse alphebetical order, \
-      then ordered by Amoun in descending order ", () => {
+describe("Test Table Sorting states", () => {
 
-    const { queryAllByTestId, queryByTestId } = render(
+  it("Table renders in user specified order", () => {
+    const { queryAllByTestId } = render(
       <Table
         title={"test"}
         gridData={sampleGridData}
@@ -148,48 +165,93 @@ describe("Active descending sort followed by reset", () => {
       "Transfer to Melanie Abrazado October 20, 2020", 
       "Transfer to Melanie Abrazado October 21, 2020"
     ]);
+  });
+
+  it("sorts by description in alphabetical order", () => {
+    const { queryAllByTestId, queryByTestId } = render(
+      <Table
+        title={"test"}
+        gridData={sampleGridData}
+        sortableHeaders={["DESCRIPTION", "AMOUNT"]}
+      />
+    );
 
     // clicking the Description Ascending order option should render the grid 
     // in alphabetical order by Description
     let sortOption = queryByTestId("DESCRIPTION_ASC");
+    let selectedColumn = queryAllByTestId("col0");
     sortOption.click();
-    columnValues = selectedColumn.map((cell) => cell.textContent);
+    let columnValues = selectedColumn.map((cell) => cell.textContent);
     expect(columnValues).toEqual([
       "Bill to Waterworks-4534 October 23, 2020",
       "Transfer to Melanie Abrazado October 20, 2020",
       "Transfer to Melanie Abrazado October 21, 2020",
       "Transfer to Noble Bank-34232 October 30, 2020"
     ]);
+  });
     
+  it("resets to original order on click", () => {
+    const { queryAllByTestId, queryByTestId } = render(
+      <Table
+        title={"test"}
+        gridData={sampleGridData}
+        sortableHeaders={["DESCRIPTION", "AMOUNT"]}
+      />
+    );
+    let sortOption = queryByTestId("DESCRIPTION_ASC");
+    let selectedColumn = queryAllByTestId("col0");
+    sortOption.click();
+    sortOption.click();
+    let columnValues = selectedColumn.map((cell) => cell.textContent);
+
     // repeating the click on the Description Ascending order option should reset 
     // the table to the original user-specified order
-    sortOption.click();
-    columnValues = selectedColumn.map((cell) => cell.textContent);
     expect(columnValues).toEqual([
       "Transfer to Noble Bank-34232 October 30, 2020", 
       "Bill to Waterworks-4534 October 23, 2020", 
       "Transfer to Melanie Abrazado October 20, 2020", 
       "Transfer to Melanie Abrazado October 21, 2020"
     ]);
+  });
+
+  it("sorts by description in reverse order", () => {
+    const { queryAllByTestId, queryByTestId } = render(
+      <Table
+        title={"test"}
+        gridData={sampleGridData}
+        sortableHeaders={["DESCRIPTION", "AMOUNT"]}
+      />
+    );
+    let sortOption = queryByTestId("DESCRIPTION_DESC");
+    let selectedColumn = queryAllByTestId("col0");
+    sortOption.click();
+    let columnValues = selectedColumn.map((cell) => cell.textContent);
 
     // clicking the Description Descending order option should render the grid in 
     // reverse alphabetical order by Description
-    sortOption = queryByTestId("DESCRIPTION_DESC");
-    sortOption.click();
-    columnValues = selectedColumn.map((cell) => cell.textContent);
     expect(columnValues).toEqual([
       "Transfer to Noble Bank-34232 October 30, 2020",
       "Transfer to Melanie Abrazado October 20, 2020",
       "Transfer to Melanie Abrazado October 21, 2020",
       "Bill to Waterworks-4534 October 23, 2020"
     ]);
+  });
+
+  it("sorts by Amount in descending order", () => {
+    const { queryAllByTestId, queryByTestId } = render(
+      <Table
+        title={"test"}
+        gridData={sampleGridData}
+        sortableHeaders={["DESCRIPTION", "AMOUNT"]}
+      />
+    );
+    let sortOption = queryByTestId("AMOUNT_DESC");
+    sortOption.click();
+    let selectedColumn = queryAllByTestId("col1");
+    let columnValues = selectedColumn.map((cell) => cell.textContent);
 
     // clicking descending option for Amount should sort the table in descending 
     // order by Amount
-    sortOption = queryByTestId("AMOUNT_DESC");
-    sortOption.click();
-    selectedColumn = queryAllByTestId("col1");
-    columnValues = selectedColumn.map((cell) => cell.textContent);
     expect(columnValues).toEqual([ 
       "-$1000", "-$130", "-$123.45", "-$80" 
     ]);

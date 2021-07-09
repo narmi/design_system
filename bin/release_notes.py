9ba6bc3911339eea3beceec7bbb3c738dd72d018
reshav-abraham <reshav@narmitech.com>
2021-07-09 15:24:02 -0400

    generate release notes from commit history

---
+++ bin/release_notes.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+import subprocess  # nosec
+import sys
+
+LATEST_COMMIT_TO_EXCLUDE = subprocess.check_output(["git", "rev-parse", "master"]).strip()
+LATEST_COMMIT_TO_INCLUDE = "HEAD"
+GIT_LOG_STRING = "git log {}..{} --pretty=oneline".format(LATEST_COMMIT_TO_EXCLUDE, LATEST_COMMIT_TO_INCLUDE)
+
+
+def create_commit_list():
+    proc = subprocess.Popen(GIT_LOG_STRING, shell=True, stdout=subprocess.PIPE,)
+    commit_messages_string = proc.communicate()[0]
+    commit_messages_array = commit_messages_string.split("\n")
+    commit_messages_cleaned = []
+    for commit_message in commit_messages_array:
+        if len(commit_message) > 0:
+            commit_messages_cleaned.append(commit_message)
+    return commit_messages_cleaned
+
+
+def get_commits(commit_list):
+    commits = []
+    for commit_message in commit_list:
+        if "Merge pull request" in commit_message:
+            continue
+        commit_message = (" ").join(commit_message.split(" ")[1:])
+        commits.append(commit_message)
+    return commits
+
+def create_release_notes(commit_messages):
+    output = ""
+    for commit_message in commit_messages:
+        formatted_message = "- " + commit_message + "\n\n"
+        output += formatted_message
+    return output
+
+
+def main():
+    commit_list = create_commit_list()
+    commits = get_commits(commit_list)
+    release_notes = create_release_notes(commits)
+    print(release_notes)
+
+
+if __name__ == "__main__":
+    main()

e44e881267676c470053c5520baefd5ce3b06677
Will Brand <will@narmitech.com>
2021-08-19 17:29:13 -0400

    Updated Mulish weights (#114)

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ const styleComponent = React.createElement("style", {
   key: "style-component"
 });
 const fontComponent = React.createElement("link", {
-  href: "https://fonts.googleapis.com/css?family=Mulish",
+  href: "https://fonts.googleapis.com/css?family=Mulish:400,600,700,400italic,700italic&subset=latin",
   rel: "stylesheet",
   key: "font-component"
 });

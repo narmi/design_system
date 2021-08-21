9698fec7f582101e821d7479af11490321804483
Will Brand <will@narmitech.com>
2021-08-21 14:00:49 -0400

    [Inputs] L: Input story, fixes for disabled and error states, extra input types. (#122)
    * Towards an Input story
    
    * Stories for inputs, designs to make inputs match styleguide
    
    * Nesting

---
+++ storybook/stories/Input.stories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../../dist/style.css';
+import { NdsStyles } from './decorators/decorators';
+import { TextInput } from 'design_system'
+export default {
+  title: 'Components/Inputs',
+  decorators: [NdsStyles],
+};
+
+export const Inputs = () => {
+  return <div className={"nds-typography"}>
+    <h1>Text Inputs</h1>
+    <div className="storybook-example-list storybook-4col nds-typography">
+      <TextInput label={"Label"} />
+      <TextInput />
+      <TextInput label={"Label"} value={"Text"} />
+      <TextInput label={"Label"} value={"Text"} error={"Error message"} />
+      <TextInput label={"Label"} value={"Disabled"} disabled />
+      <TextInput label={"Phone number"} value={"(555) 867-5309"} type={"tel"} />
+      <TextInput label={"Date"} value={"1989-01-10"} type={"date"} />
+    </div>
+  </div>
+}
--- storybook/stories/decorators/decorators.js
+++ storybook/stories/decorators/decorators.js
@@ -6,6 +6,12 @@ export const NdsStyles = (Story) => (
           :root {
             --nds-primary-color: red;
             --nds-font-family: 'Mulish';
+            --nds-primary-color: 17, 59, 94;
+            --nds-secondary-color: 223, 52, 22;
+            --nds-text-header: 51, 51, 51;
+            --nds-disabled-background: 243, 243, 243;
+            --nds-disabled: 140, 140, 140;
+            --nds-error: 233, 59, 59;
             };
           `}
         </style>

4c6b884a337384ab32d2c7a28f51b4192fd9d477
Reshav Abraham <23458204+reshav-abraham@users.noreply.github.com>
2021-08-23 15:25:06 -0400

    [Modal] L: Modal converted to uses scss, added minimum Modal Story (#139)
    * center modal
    
    * Modal scss
    
    * scss cleanup
    
    * scss cleanup
    
    * cleanup
    
    * cleanup

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { X } from "react-feather";
-import { deviceBreakpoints } from "global";
-
 export const modalZIndex = 100;
 
 const Modal = ({ open, setOpen, children, type }) => {
@@ -14,7 +11,7 @@ const Modal = ({ open, setOpen, children, type }) => {
     />
     <div className="nds-modal-container">
       <div className="nds-modal-dismiss" onClick={() => {setOpen(false)}}>
-        <X/>
+        <span className={"narmi-icon-x"} style={{color: "#333333"}}/>
       </div>
       {children}
     </div>
@@ -33,70 +30,5 @@ Modal.defaultProps = {
   setOpen: () => {},
   type: "center",
 };
-
-Modal.styles = `
-  .nds-modal .nds-modal-overlay {
-    display: none;
-    z-index: ${modalZIndex};
-    background: #000;
-    opacity: 70%;
-    position: fixed;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-  }
-  .nds-modal.open > .nds-modal-overlay {
-    display: block;
-  }
-
-  .nds-modal .nds-modal-dismiss {
-    display: none;
-    position: absolute;
-    top: 18px;
-    right: 18px;
-  }
-  .nds-modal.open > .nds-modal-container > .nds-modal-dismiss {
-    display: block;
-  }
-  .nds-modal > .nds-modal-container {
-    display: none;
-    z-index: ${modalZIndex};
-    position: fixed;
-    background: rgb(var(--nds-background));
-  }
-  .nds-modal.open > .nds-modal-container {
-    display: block;
-  }
-
-  /* right styles */
-  .nds-modal.right > .nds-modal-container {
-    top: 0;
-    right: 0;
-    bottom: 0;
-    min-width: 335px;
-    padding: 40px 25px 40px 20px;
-  }
-
-  /* details styles */
-  .nds-modal.details.open .nds-modal-overlay {
-    display: none;
-  }
-  .nds-modal.details.open > .nds-modal-container > .nds-modal-dismiss {
-    display: none;
-  }
-  .nds-modal.details > .nds-modal-container {
-    position: static;
-  }
-  @media ${`(min-width: ${deviceBreakpoints.desktop})`} {
-    .nds-modal.details.open .nds-modal-overlay {
-      display: block;
-      opacity: 0;
-    }
-    .nds-modal.details > .nds-modal-container {
-      position: fixed;
-    }
-  }
-`;
 
 export default Modal;
---
+++ storybook/stories/Modal.stories.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { Modal, Button } from 'design_system';
+import { NdsStyles } from './decorators/decorators';
+
+export default {
+  title: 'Components/Modal',
+  decorators: [NdsStyles],
+};
+
+export const SampleModal = () => {
+    const [open, setOpen] = useState(false);
+    return ( 
+    <div className="nds-typography">
+        <Button onClick={()=>{setOpen(true)}}>Open Modal</Button>
+        <Modal
+            open={open}
+            setOpen={setOpen}
+        >
+            <div style={{width: "500px"}}>
+                <h4>Title</h4>
+                <p>
+                    This is an example of body text that a user could pass
+                </p>
+            </div>
+    </Modal> 
+  </div>
+  )
+}

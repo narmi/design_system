09066fb84dbc3042c4ebb1fadeea834e9c8f6e82
reshav-abraham <reshav@narmitech.com>
2021-08-24 11:16:04 -0400

    cleanup

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
 const Modal = ({ open, handleClose, children, classes, header, actions }) => {

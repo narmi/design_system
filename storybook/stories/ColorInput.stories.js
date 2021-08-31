ff37a57affc8f76105460df222b646718f8cf37b
Will Brand <will@narmitech.com>
2021-08-31 11:22:18 -0400

    Storybook spacing

--- storybook/stories/ColorInput.stories.js
+++ storybook/stories/ColorInput.stories.js
@@ -11,11 +11,9 @@ export const ColorInput = () => {
   const [color, setColor] = useState("#915F6D")
   return <div className={"nds-typography"}>
     <h1 style={{marginBottom: "1em"}}>Edit Primary Accessible Color</h1>
-    <p>Note: This color will not update in real time and you'll need
-    to reach out to Narmi at (email) to request a deploy</p>
     <div style={{display: "flex", alignItems: "center"}}>
       <TextInput label={"Hex Code"} defaultValue="915F6D" onChange={(e)=>{setColor("#" + e.target.value);}}/> 
-      <span className="narmi-icon-solid-circle" style={{marginLeft: "30px", color:color, fontSize:"50px"}}></span>
+      <span className="narmi-icon-solid-circle" style={{marginLeft: "20px", color:color, fontSize:"50px"}}></span>
     </div>
   </div>
 }

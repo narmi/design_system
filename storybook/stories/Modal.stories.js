f82c1f5abf063a5c33760ed7dabc1c04c003e89a
Reshav Abraham <23458204+reshav-abraham@users.noreply.github.com>
2021-08-31 11:10:46 -0400

    [Modal, RadioButton] L: modal stories, resolve radiobutton warning (#143)
    * modal stories, resolve radiobutton warning passing invalid prop to html element
    
    * Story
    
    Co-authored-by: Will Brand <will@narmitech.com>

--- storybook/stories/Modal.stories.js
+++ storybook/stories/Modal.stories.js
@@ -1,28 +1,83 @@
-import React, { useState } from 'react';
-import { Modal, Button } from 'design_system';
-import { NdsStyles } from './decorators/decorators';
+import React, { useState } from "react";
+import { Modal, Button, PlainButton, RadioButtons } from "design_system";
+import { NdsStyles } from "./decorators/decorators";
 
 export default {
-  title: 'Components/Modal',
+  title: "Components/Modal",
   decorators: [NdsStyles],
 };
 
-export const SampleModal = () => {
-    const [open, setOpen] = useState(false);
-    return ( 
+export const SimpleModal = () => {
+  const [open, setOpen] = useState(false);
+  return (
     <div className="nds-typography">
-        <Button onClick={()=>{setOpen(true)}}>Open Modal</Button>
-        <Modal
-            open={open}
-            setOpen={setOpen}
-        >
-            <div style={{width: "500px"}}>
-                <h4>Title</h4>
-                <p>
-                    This is an example of body text that a user could pass
-                </p>
-            </div>
-    </Modal> 
-  </div>
-  )
-}
+      <Button
+        onClick={() => {
+          setOpen(true);
+        }}
+      >
+        Open Modal
+      </Button>
+      <Modal 
+        open={open}
+        handleClose={()=>{setOpen(false)}}
+        header={"Title"}
+      >
+        <div style={{ width: "500px" }}>
+          <p>This is an example of body text that a user could pass</p>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export const AccountModal = () => {
+  const [open, setOpen] = useState(true);
+  const [deleteTransaction, setDeleteTransaction] = useState(false);
+  const actions = (
+    <>
+      <PlainButton
+        onClick={() => { setOpen(false); }}
+        style={{ paddingRight: "16px", color: "rgb(var(--nds-primary-color))" }}
+        type="plain"
+      >
+        Cancel
+      </PlainButton>
+      <Button
+        disabled={deleteTransaction === false}
+        onClick={() => { setOpen(false); }}
+      >Yes, Delete</Button>
+    </>
+  );
+  return (
+    <div className="nds-typography">
+      <Button
+        onClick={() => {
+          setOpen(true);
+        }}
+      >
+        Open Modal
+      </Button>
+      <Modal
+        open={open}
+        actions={actions}
+        handleClose={()=>{setOpen(false)}}
+        header={"Delete your upcoming transaction"}
+      >
+        <div style={{ width: "500px" }}>
+          <div>
+            <p>This is an example of body text that a user could pass</p>
+            <RadioButtons
+              onChange={(e) => {
+                setDeleteTransaction(e.target.value === "false" ? false : true);
+              }}
+              initialValue={false}
+              name="setting_value"
+              options={{ True: true, False: false }}
+            />
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+};

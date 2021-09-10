011db565ab87e34b2e219e5682cf92d1e64cda23
reshav-abraham <reshav@narmitech.com>
2021-09-10 09:24:43 -0400

    dropdown component

---
+++ src/Dropdown.jsx
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from "react";
+import TextInput from "./TextInput";
+import PropTypes from "prop-types";
+
+const Chevron = ({ open, setOpen }) => {
+  if (open) {
+    return (
+      <span
+        className={"nds-dropdown-chevron narmi-icon-chevron-up"}
+        onClick={setOpen.bind(null, false)}
+      />
+    );
+  }
+  return (
+    <span
+      onClick={setOpen.bind(null, true)}
+      className={"nds-dropdown-chevron narmi-icon-chevron-down"}
+    />
+  );
+};
+Chevron.propTypes = {
+  open: PropTypes.bool,
+  setOpen: PropTypes.func,
+};
+
+const DropdownMenu = ({ open, onClose, children, ...rest }) => {
+  /*
+   * Expand and display the dropdown options, if open=true
+   */
+  return open ? (
+    <div className="nds-dropdown-menu">
+      <div className="nds-dropdown-overlay" onClick={onClose}></div>
+      <div
+        className="nds-dropdown-menu-items"
+        aria-modal
+        aria-hidden
+        tabIndex={-1}
+        role="dialog"
+        {...rest}
+      >
+        {children}
+      </div>
+    </div>
+  ) : null;
+};
+
+const Dropdown = (props) => {
+  const [open, setOpen] = useState(props.defaultOpen);
+  const [value, setValue] = useState(undefined);
+
+  useEffect(() => {
+    if (value === undefined && props.defaultValue) {
+      setValue(props.defaultValue);
+    }
+  });
+  useEffect(() => {
+    if (props.closeDropDown === true) {
+      closeDropdown();
+    }
+  }, [props.closeDropDown]);
+
+  const openDropdown = () => {
+    setOpen(true);
+  };
+  const closeDropdown = () => {
+    setOpen(false);
+  };
+
+  const childrenList = Array.isArray(props.children)
+    ? props.children.flat()
+    : props.children.flat();
+
+  const { multiline, style, ...nativeElementProps } = props;
+  return (
+    <div
+      onKeyDown={(event) => {
+        if (["Tab", "Escape"].includes(event.key)) closeDropdown();
+      }}
+      className="nds-dropdown"
+      field="test"
+    >
+      <TextInput
+        onClick={() => {
+          setOpen(true);
+        }}
+        value={value}
+        field={props.field}
+        label={props.triggerLabel}
+      />
+      <DropdownMenu open={open} onClose={closeDropdown}>
+        <div className="nds-dropdown-children nds-typography">
+          {childrenList.map((child, i) =>
+            React.cloneElement(child, {
+              className: "nds-dropdown-item",
+              onClick: child.props.onClick
+                ? child.props.onClick
+                : () => {
+                    setValue(child.props.children);
+                    props.onChange(child.props.children);
+                    closeDropdown();
+                  },
+              key: i,
+            })
+          )}
+        </div>
+      </DropdownMenu>
+      <Chevron open={open} setOpen={setOpen} />
+    </div>
+  );
+};
+
+Dropdown.defaultProps = {
+  defaultOpen: false,
+  onChange: () => {
+    null;
+  },
+};
+Dropdown.propTypes = {
+  children: PropTypes.any,
+  defaultOpen: PropTypes.bool,
+  onChange: PropTypes.func,
+  closeDropDown: PropTypes.bool,
+  triggerLabel: PropTypes.string.isRequired,
+  triggerValue: PropTypes.string,
+};
+
+export default Dropdown;
---
+++ storybook/stories/Dropdown.stories.js
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import { Modal, PlainButton, Dropdown, TextInput, Button } from "design_system";
+import { NdsStyles } from "./decorators/decorators";
+
+export default {
+  title: "Components/Dropdown",
+  decorators: [NdsStyles],
+};
+
+export const BasicDropdown = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  return (
+    <div>
+      <Dropdown triggerLabel={"Accounts"} closeDropDown={modalOpen}>
+        <div>Rowena Wick</div>
+        <div>Daya Zakim</div>
+        <PlainButton
+          onClick={() => {
+            setModalOpen(true);
+          }}
+          style={{ color: "rgb(var(--nds-primary-color))" }}
+        >
+          Link a new payee!
+        </PlainButton>
+      </Dropdown>
+      <Modal
+        open={modalOpen}
+        handleClose={() => {
+          setModalOpen(false);
+        }}
+      >
+        <div style={{ width: "500px" }}>
+          <h4>Title</h4>
+          <p>This is an example of body text that a user could pass</p>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export const NewMemberDropDown = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const actions = (
+      <div className="nds-typography">
+        <PlainButton
+          onClick={() => { setModalOpen(false); }}
+          style={{ paddingRight: "16px", color: "rgb(var(--nds-primary-color))" }}
+          type="plain"
+        >
+          Cancel
+        </PlainButton>
+        <Button
+          onClick={() => { setModalOpen(false); }}
+        >Add member</Button>
+      </div>
+    );
+  return (
+    <div>
+      <Dropdown defaultValue={"test"} triggerLabel={"Members"} closeDropDown={modalOpen}>
+        {["test", "test2"].map((option) => (
+          <div>{option}</div>
+        ))} 
+        <PlainButton
+          onClick={() => {
+            setModalOpen(true);
+          }}
+          style={{ color: "rgb(var(--nds-primary-color))" }}
+        >
+          Add a new member
+        </PlainButton>
+      </Dropdown>
+      <Modal
+        open={modalOpen}
+        handleClose={() => {
+          setModalOpen(false);
+        }}
+        header={"Add a new member"}
+        actions={actions}
+      >
+        <div style={{ width: "500px" }} className="nds-typography">
+          <TextInput label={"First Name"} style={{paddingTop: "20px", paddingBottom: "20px"}}/>
+          <TextInput label={"Last Name"} style={{paddingBottom: "20px"}}/>
+          <TextInput label={"Account Number"} style={{paddingBottom: "20px"}}/>
+          <TextInput label={"Relationship (i.e. sister)"} style={{paddingBottom: "20px"}}/>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+
+export const Inputs = () => {
+    return(
+        <div style={{ width: "500px" }} className="nds-typography">
+          <TextInput
+            label={"First Name"}
+            style={{ paddingBottom: "20px" }}
+          />
+          <TextInput label={"Last Name"} style={{ paddingBottom: "20px" }} />
+          <TextInput
+            label={"Account Number"}
+            style={{ paddingBottom: "20px" }}
+          />
+          <TextInput
+            label={"Relationship (i.e. sister)"}
+            style={{ paddingBottom: "20px" }}
+          />
+        </div>
+        );
+      }

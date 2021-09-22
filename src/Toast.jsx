1e4c7f9beea2ff7487bc843dc4764004874c944b
reshav-abraham <reshav@narmitech.com>
2021-09-22 09:10:04 -0400

    clean up

--- src/Toast.jsx
+++ src/Toast.jsx
@@ -3,8 +3,9 @@ import PropTypes from "prop-types";
 
 const Toast = (props) => {
   const [open, setOpen] = React.useState(props.open);
-
+  const toastIcons = {"success": "check", "error": "error", "info": "info"}
   const [seconds, setSeconds] = React.useState(0);
+
   React.useEffect(() => {
     if (props.open) {
       setOpen(props.open);
@@ -21,23 +22,14 @@ const Toast = (props) => {
     }
   }, [seconds]);
 
-  const iconType =
-    props.type === "success"
-      ? "check"
-      : props.type === "error"
-      ? error
-      : props.type === "info"
-      ? "info"
-      : "";
-
   return (
     <div
       style={props.style}
-      className={`nds-toast-${props.type} nds-toast-${
-        open ? "active" : "hidden"
+      className={`nds-toast-${props.type || ""} nds-toast-${
+        open ? "" : "hidden"
       }`}
     >
-      <span className={`narmi-icon-${iconType}`} />
+      <span className={`narmi-icon-${toastIcons[props.type] || ""}`} />
       <span> {props.message} </span>
     </div>
   );

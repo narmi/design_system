4f4bda3f02e86f5cb66cd22fcc3e7251a3c8d497
reshav-abraham <reshav@narmitech.com>
2021-10-04 14:58:28 -0400

    basic jest text input test

---
+++ __tests__/TextInput.test.jsx
@@ -0,0 +1,21 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react";
+import { render, screen, getByRole } from "@testing-library/react";
+import TextInput from "../src/TextInput";
+
+
+describe('TextInput', () => {
+  it('Basic Test to verify TextInput behaves as an Html input', () => {
+    const editScreen = render(
+       <TextInput id="textinput" label={"Label"} defaultValue={"Default"} />,
+    );
+    const label = screen.getByLabelText("Label");
+    expect(label.value).toEqual('Default');
+
+    label.value = "User input text";
+    expect(label.value).toEqual('User input text');
+  });
+});

d7dd43acbf6eb01356fb4a2ea2e1ec850633f154
akdetrick <adam.detrick@narmitech.com>
2021-10-15 16:00:16 -0400

    feat(DateInput): add `disableDays` prop for disabling dates

--- src/DateInput.jsx
+++ src/DateInput.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from "react";
+import React, { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import Input from "Input";
 import { english } from "flatpickr/dist/l10n/default";
@@ -6,11 +6,19 @@ import flatpickr from "flatpickr";
 import moment from "moment";
 import "flatpickr/dist/themes/airbnb.css";
 
-const DateInput = ({ minDate, altInput, altFormat, ...props }) => {
+const DateInput = ({
+  disableDates,
+  minDate,
+  altInput,
+  altFormat,
+  ...props
+}) => {
   const input = useRef();
 
   // Shorten "Sun" to "Su", "Mon" to "Mo", etc.
-  english.weekdays.shorthand = english.weekdays.shorthand.map((abbrev) => abbrev.substr(0, 2))
+  english.weekdays.shorthand = english.weekdays.shorthand.map((abbrev) =>
+    abbrev.substr(0, 2)
+  );
   const flatpickrOptions = {
     monthSelectorType: "static",
     locale: english,
@@ -19,25 +27,37 @@ const DateInput = ({ minDate, altInput, altFormat, ...props }) => {
     minDate,
     altInput,
     altFormat,
-    onChange: (flatpickrVal) => props.onChange(moment(flatpickrVal[0]).format("YYYY-MM-DD"))
-  }
+    disable: disableDates,
+    onChange: (flatpickrVal) =>
+      props.onChange(moment(flatpickrVal[0]).format("YYYY-MM-DD")),
+  };
 
   useEffect(() => {
-    flatpickr(input.current, flatpickrOptions)
+    flatpickr(input.current, flatpickrOptions);
   });
 
   return (
     <Input {...props}>
-      <input key={"nds-text"} ref={input} type="date" required placeholder={props.label} {...props} />
+      <input
+        key={"nds-text"}
+        ref={input}
+        type="date"
+        required
+        placeholder={props.label}
+        {...props}
+      />
     </Input>
   );
 };
 DateInput.propTypes = {
   onChange: PropTypes.func,
   label: PropTypes.string,
+  /** Disables dates. See [flatpickr docs](https://flatpickr.js.org/examples/#disabling-dates) for usage instructions */
+  disableDates: PropTypes.array,
 };
 DateInput.defaultProps = {
-  onChange: () => {}
+  onChange: () => {},
+  disableDates: [],
 };
 
 export default DateInput;
--- storybook/stories/Input.stories.js
+++ storybook/stories/Input.stories.js
@@ -1,46 +1,77 @@
-import React, {useState} from 'react';
-import { NdsStyles } from './decorators/decorators';
-import { DateInput, TextInput } from 'design_system';
+import React, { useState } from "react";
+import { NdsStyles } from "./decorators/decorators";
+import { DateInput, TextInput } from "design_system";
 
 export default {
-  title: 'Components/Inputs',
+  title: "Components/Inputs",
   decorators: [NdsStyles],
 };
 
 export const Inputs = () => {
-  return <div className={"nds-typography"}>
-    <h1 style={{marginBottom: "1em"}}>Text Inputs</h1>
-    <div className="storybook-4col">
-      <TextInput label={"Label"} />
-      <TextInput />
-      <TextInput label={"Label"} defaultValue={"Text"} />
-      <TextInput label={"Label"} defaultValue={"Text"} error={"Error message"} />
-      <TextInput label={"Label"} defaultValue={"Disabled"} disabled />
-      <TextInput label={"Phone number"} defaultValue={"(555) 867-5309"} type={"tel"} />
-      <TextInput label={"Test Quotes"} formatter={(text)=>text.replace("“",'"').replace("”",'"')}/>
-      <form>
-        <TextInput label={"Secret phrase"} type="password" autoComplete="on"/>
-      </form>
+  return (
+    <div className={"nds-typography"}>
+      <h1 style={{ marginBottom: "1em" }}>Text Inputs</h1>
+      <div className="storybook-4col">
+        <TextInput label={"Label"} />
+        <TextInput />
+        <TextInput label={"Label"} defaultValue={"Text"} />
+        <TextInput
+          label={"Label"}
+          defaultValue={"Text"}
+          error={"Error message"}
+        />
+        <TextInput label={"Label"} defaultValue={"Disabled"} disabled />
+        <TextInput
+          label={"Phone number"}
+          defaultValue={"(555) 867-5309"}
+          type={"tel"}
+        />
+        <TextInput
+          label={"Test Quotes"}
+          formatter={(text) => text.replace("“", '"').replace("”", '"')}
+        />
+        <form>
+          <TextInput
+            label={"Secret phrase"}
+            type="password"
+            autoComplete="on"
+          />
+        </form>
+      </div>
     </div>
-  </div>
-}
+  );
+};
 
 export const DateInputs = () => {
-  return <div className={"nds-typography"}>
-    <h1 style={{marginBottom: "1em"}}>Date Pickers</h1>
-    <div className="storybook-4col">
-      <DateInput label={"Date of Birth"} />
+  return (
+    <div className={"nds-typography"}>
+      <h1 style={{ marginBottom: "1em" }}>Date Pickers</h1>
+      <div className="storybook-4col">
+        <DateInput label={"Date of Birth"} disableDates={["2021-10-22"]} />
+      </div>
     </div>
-  </div>
-}
+  );
+};
 
 export const ColorInput = () => {
   const [color, setColor] = useState("#915F6D");
-  return <div className={"nds-typography"}>
-    <h1 style={{marginBottom: "1em"}}>Edit Primary Accessible Color</h1>
-    <div style={{display: "flex", alignItems: "center"}}>
-      <TextInput label={"Hex Code"} defaultValue="915F6D" formatter={(text)=>text.replace("#",'')} onBlur={(e)=>{setColor("#" + e.target.value)}} />
-      <span className="narmi-icon-solid-circle" style={{marginLeft: "20px", color:color, fontSize:"50px"}}></span>
+  return (
+    <div className={"nds-typography"}>
+      <h1 style={{ marginBottom: "1em" }}>Edit Primary Accessible Color</h1>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <TextInput
+          label={"Hex Code"}
+          defaultValue="915F6D"
+          formatter={(text) => text.replace("#", "")}
+          onBlur={(e) => {
+            setColor("#" + e.target.value);
+          }}
+        />
+        <span
+          className="narmi-icon-solid-circle"
+          style={{ marginLeft: "20px", color: color, fontSize: "50px" }}
+        ></span>
+      </div>
     </div>
-  </div>
-}
+  );
+};

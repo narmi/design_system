d192915a7eaf698e51935e597a62681dfec7f625
akdetrick <adam.detrick@narmitech.com>
2021-10-28 17:53:02 -0400

    chore: run prettier on all files

--- babel.config.js
+++ babel.config.js
@@ -10,7 +10,5 @@ module.exports = {
     ],
     ["@babel/preset-react"],
   ],
-  plugins: [
-    "@babel/plugin-proposal-class-properties",
-  ],
+  plugins: ["@babel/plugin-proposal-class-properties"],
 };

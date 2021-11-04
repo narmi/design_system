dd9aefad18c1110d7ce42065997b24b5ceab0a8d
akdetrick <adam.detrick@narmitech.com>
2021-11-04 17:51:10 -0400

    feat(Dialog): add `Dialog` component

---
+++ src/Dialog/index.stories.js
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import Dialog from "./";
+import Button from "../Button";
+import { DialogLayout } from "../../.storybook/Layouts";
+
+const BaseTemplate = (args) => <Dialog {...args} />;
+const InteractiveTempalte = (args) => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  return (
+    <>
+      <Button
+        onClick={() => {
+          setIsDialogOpen(true);
+        }}
+      >
+        Open Dialog
+      </Button>
+      <Dialog
+        isOpen={isDialogOpen}
+        onUserDismiss={() => {
+          setIsDialogOpen(false);
+        }}
+        footer={
+          <div style={{ textAlign: "right" }}>
+            <Button
+              onClick={() => {
+                setIsDialogOpen(false);
+              }}
+            >
+              Close Dialog
+            </Button>
+          </div>
+        }
+        {...args}
+      />
+    </>
+  );
+};
+
+export const Overview = BaseTemplate.bind({});
+Overview.args = {
+  isOpen: false,
+  title: "Dialog title",
+  children: <p>Dialog content</p>,
+  footer: (
+    <div style={{ textAlign: "right" }}>
+      <Button>Accept</Button>
+    </div>
+  ),
+  headerStyle: "bordered",
+  onUserDismiss: () => {},
+  width: "500px",
+};
+
+export const UsingWithState = InteractiveTempalte.bind({});
+UsingWithState.args = {
+  title: "Dialog controlled by external state",
+  children: <p>Dialog content</p>,
+};
+UsingWithState.parameters = {
+  docs: {
+    description: {
+      story:
+        "The `Dialog` will render open our closed based on the `isOpen` prop passed in. Use the `onUserDismiss` callback to determine when to change the value of `isOpen`.",
+    },
+  },
+};
+
+export const ScrollingContent = InteractiveTempalte.bind({});
+ScrollingContent.args = {
+  hasLongContent: true,
+  title: "Resize your window",
+  children: (
+    <>
+      <p>
+        This content area will become scrollable whenever the content overflows
+        its container.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet. Ea fugiat dolore quo possimus adipisci est
+        ipsum libero ab dolores minima ut facere rerum? Aut vitae sint ut nemo
+        nisi ut tempore voluptas. Eum adipisci quasi eum praesentium libero est
+        quidem consequatur At voluptatum debitis et laborum ducimus aut eaque
+        eligendi.
+      </p>
+      <p>
+        Ut alias eligendi ut dolorem eius rem consectetur ullam et natus nihil.
+        Et maiores dolores hic nesciunt quibusdam ut laboriosam earum qui quas
+        sapiente in molestiae accusantium.
+      </p>
+      <p>
+        Ut ducimus amet quo deleniti repellendus in illo eaque 33 nihil quis
+        eveniet deleniti qui sapiente quia At repellendus veritatis. Qui
+        voluptatem culpa et fugit debitis ut fugit quidem sit omnis deserunt qui
+        sequi placeat. Non voluptatem molestiae et explicabo voluptas ut facilis
+        quia?
+      </p>
+    </>
+  ),
+};
+ScrollingContent.parameters = {
+  docs: {
+    description: {
+      story:
+        "The `Dialog` will grow in height to fit content until it reaches edges of the viewport, at which point, the content area will become scrollable and a gradient border appears betwen the footer and content area.",
+    },
+  },
+};
+
+export default {
+  title: "Components/Dialog",
+  component: Dialog,
+  parameters: {
+    docs: {
+      page: DialogLayout,
+    },
+  },
+};

70d99feebfda7905f55d79e109a2dfc80fd324f6
akdetrick <adam.detrick@narmitech.com>
2021-12-03 17:37:18 -0500

    feat(Tabs): add Tabs component

---
+++ src/Tabs/TabsList.js
@@ -0,0 +1,56 @@
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
+import TabsContext from "./context";
+
+const noop = () => {};
+
+const TabsList = ({ children }) => {
+  const { tabIds, setTabIds, changeTabs, selectedIndex, hasPanels } =
+    useContext(TabsContext);
+  const childArray = React.Children.toArray(children);
+
+  // populate tabIds state variable in root component
+  // with tabId props from `Tabs.Tab` children passed into `Tabs.List`
+  if (tabIds.length !== childArray.length) {
+    setTabIds(childArray.map((t) => t.props.tabId));
+  }
+
+  const handleKeyDown = ({ key }) => {
+    let newIndex;
+    switch (key) {
+      case "ArrowLeft":
+        newIndex = selectedIndex - 1;
+        if (newIndex >= 0) {
+          changeTabs(tabIds[newIndex]);
+        }
+        break;
+      case "ArrowRight":
+        newIndex = selectedIndex + 1;
+        if (newIndex <= tabIds.length - 1) {
+          changeTabs(tabIds[newIndex]);
+        }
+        break;
+    }
+  };
+
+  return (
+    <ul
+      role={hasPanels ? "tablist" : undefined}
+      className="list--reset nds-tabs-tabsList"
+      onKeyDown={hasPanels ? handleKeyDown : noop}
+      tabIndex={hasPanels ? "0" : undefined}
+      data-testid="nds-tablist"
+    >
+      {children}
+    </ul>
+  );
+};
+
+TabsList.propTypes = {
+  /** Children must be of type `Tabs.Tab` */
+  children: PropTypes.node.isRequired,
+};
+
+TabsList.displayName = "Tabs.List";
+
+export default TabsList;
---
+++ src/Tabs/index.stories.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Tabs from "./";
+
+const Template = (args) => (
+  <Tabs {...args}>
+    <Tabs.List>
+      <Tabs.Tab label="Apples" tabId="apple" />
+      <Tabs.Tab label="Oranges" tabId="orange" />
+      <Tabs.Tab label="Pineapples" tabId="pineapple" />
+    </Tabs.List>
+    <Tabs.Panel tabId="apple">
+      <div className="padding--all--s">🍎🍎🍎</div>
+    </Tabs.Panel>
+    <Tabs.Panel tabId="orange">
+      <div className="padding--all--s">🍊🍊🍊</div>
+    </Tabs.Panel>
+    <Tabs.Panel tabId="pineapple">
+      <div className="padding--all--s">🍍🍍🍍</div>
+    </Tabs.Panel>
+  </Tabs>
+);
+
+export const Overview = Template.bind({});
+Overview.argTypes = {
+  onTabChange: { action: "tab change" },
+};
+
+export const DefaultSelectedTab = Template.bind({});
+DefaultSelectedTab.args = {
+  defaultSelectedIndex: 1,
+};
+
+export const WithoutPanels = () => (
+  <Tabs>
+    <Tabs.List>
+      <Tabs.Tab label="Apples" tabId="apple" />
+      <Tabs.Tab label="Oranges" tabId="orange" />
+      <Tabs.Tab label="Pineapples" tabId="pineapple" />
+    </Tabs.List>
+  </Tabs>
+);
+WithoutPanels.parameters = {
+  docs: {
+    description: {
+      story:
+        "You can decouple tabs from conent by omitting the panel components. Use the `onTabChange` callback to respond to user events.",
+    },
+  },
+};
+
+export default {
+  title: "Components/Tabs",
+  component: Tabs,
+};

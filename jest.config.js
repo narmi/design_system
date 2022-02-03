fedf1b9fd8997c8a482945228cb0809a39fda191
akdetrick <adam.detrick@narmitech.com>
2022-02-03 17:55:56 -0500

    test(index): basic example consumer unit test

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   moduleFileExtensions: ["js", "json", "jsx", "vue"],
   moduleDirectories: ["node_modules", "src"],
   testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: ["./setupTests.js"],
+  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+  moduleNameMapper: {
+    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js",
+  },
 };

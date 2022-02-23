e794182677dbe5cdb3fee969f0d696fc895a6f4a
Alexander <alex@narmi.com>
2022-02-22 17:15:08 -0800

    fix(Dialog): more robustly check if a click event target is not inside the Dialog, before calling dismiss method.

--- src/Dialog/index.js
+++ src/Dialog/index.js
@@ -62,15 +62,16 @@ const Dialog = ({
     };
   }, [handleKeyDown, checkContentOverflow, isOpen]);
 
-  const handleClick = (event) => {
-    const isClickInside = dialogRef?.current?.contains(event?.target);
+  const handleClick = React.useCallback((event) => {
+    if (!document.body.contains(event.target)) return;
+    const isClickInside = dialogRef.current?.contains(event.target);
     if (isOpen && !isClickInside) onUserDismiss();
-  };
+  }, [isOpen]);
 
   useEffect(() => {
-    window.addEventListener("click", handleClick);
-    return () => window.removeEventListener("click", handleClick);
-  }, [isOpen]);
+    document.addEventListener("click", handleClick);
+    return () => document.removeEventListener("click", handleClick);
+  }, [handleClick]);
 
   const dialogJSX = (
     <div className="nds-shim--dark">

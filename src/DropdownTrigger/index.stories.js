678ee0be629a4b119e58fc3e257a587376986116
akdetrick <adam.detrick@narmitech.com>
2022-03-23 19:42:29 -0400

    feat(DropdownTrigger): add component for triggering popovers

---
+++ src/DropdownTrigger/index.stories.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import DropdownTrigger from "./";
+import Popover from "../Popover";
+import RadioButtons from "../RadioButtons";
+
+const Template = (args) => <DropdownTrigger {...args} />;
+
+export const Overview = Template.bind({});
+Overview.args = {
+  labelText: "State",
+};
+
+export const LabelOnly = () => <DropdownTrigger labelText="Filters (6)" />;
+LabelOnly.parameters = {
+  docs: {
+    description: {
+      story:
+        "The `displayValue` can be omitted if you only need to render a label",
+    },
+  },
+};
+
+export const ErrorState = () => (
+  <DropdownTrigger
+    labelText="Account"
+    displayValue="Checking (1234)"
+    errorText="This account is not eligible"
+  />
+);
+ErrorState.parameters = {
+  docs: {
+    description: {
+      story: "Pass `errorText` to enable the error state of `DropdownTrigger`",
+    },
+  },
+};
+
+export const ComposingWithPopover = () => {
+  const [fondueType, setFondueType] = useState("");
+
+  const popoverContent = (
+    <div className="padding--top padding--x">
+      <RadioButtons
+        name="fondueType"
+        options={{
+          Chocolate: "Chocolate",
+          Cheese: "Cheese",
+        }}
+        onChange={({ target }) => {
+          setFondueType(target.value);
+        }}
+      />
+    </div>
+  );
+
+  return (
+    <>
+      <Popover content={popoverContent} matchTriggerWidth>
+        <DropdownTrigger
+          labelText="Fondue Preference"
+          displayValue={fondueType}
+          minWidth="340px"
+        />
+      </Popover>
+    </>
+  );
+};
+ComposingWithPopover.parameters = {
+  docs: {
+    description: {
+      story:
+        "The `DropdownTrigger` component was designed to be composed with `Popover` or any other modal UI element. In this example, `DropdownTrigger` is used as a triggering element for the `Popover` component.",
+    },
+  },
+};
+
+export default {
+  title: "Components/DropdownTrigger",
+  component: DropdownTrigger,
+  argTypes: {
+    displayValue: {
+      options: ["", "NY", "CA"],
+    },
+  },
+};
---
+++ src/DropdownTrigger/index.test.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { render, screen, prettyDOM } from "@testing-library/react";
+import DropdownTrigger from "./";
+
+describe("DropdownTrigger", () => {
+  it("Renders with default props as expected", () => {
+    render(<DropdownTrigger labelText="Account" />);
+    expect(screen.getByText("Account")).toBeInTheDocument();
+    expect(screen.queryByRole("img")).toBeInTheDocument();
+  });
+
+  it("Does NOT show label when `labelText` is not passed", () => {
+    render(<DropdownTrigger labelText="Account" />);
+    expect(screen.getByText("Account")).toBeInTheDocument();
+  });
+
+  it("Moves label into floating position when a displayValue is passed", () => {
+    render(<DropdownTrigger labelText="Account" displayValue="Value" />);
+    expect(screen.getByTestId("dropdownTriggerButton")).toHaveClass(
+      "nds-dropdownTrigger-button--hasValue"
+    );
+  });
+
+  it("Does NOT show open indicator when showOpenIndicator is set to false", () => {
+    render(<DropdownTrigger labelText="Account" showOpenIndicator={false} />);
+    expect(screen.queryByRole("img")).not.toBeInTheDocument();
+  });
+
+  it("Shows correct label and icon in closed state", () => {
+    render(<DropdownTrigger labelText="Account" />);
+    expect(screen.queryByRole("img")).toHaveClass("narmi-icon-chevron-down");
+  });
+
+  it("Shows correct label and icon in open state", () => {
+    render(<DropdownTrigger labelText="Account" isOpen />);
+    expect(screen.queryByRole("img")).toHaveClass("narmi-icon-chevron-up");
+  });
+
+  it("Renders error state correctly when `errorText` is passed", () => {
+    render(<DropdownTrigger labelText="Account" errorText="You did an oops" />);
+    expect(screen.getByText("You did an oops")).toBeInTheDocument();
+  });
+
+  it("spreads labelProps on label element correctly", () => {
+    render(
+      <DropdownTrigger
+        labelText="Account"
+        labelProps={{
+          htmlFor: "somefield",
+        }}
+      />
+    );
+    expect(screen.getByText("Account")).toHaveAttribute("for", "somefield");
+  });
+
+  it("spreads extra props onto the button element", () => {
+    render(<DropdownTrigger labelText="Account" aria-haspopup="true" />);
+    expect(screen.getByTestId("dropdownTriggerButton")).toHaveAttribute(
+      "aria-haspopup",
+      "true"
+    );
+  });
+});

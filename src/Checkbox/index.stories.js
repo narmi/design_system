75ce0d048e56d8c64fe48f08c26641cdf6a2033b
akdetrick <adam.detrick@narmitech.com>
2022-03-30 16:03:02 -0400

    fix(Checkbox): enable keyboard navigation for a11y

--- src/Checkbox/index.stories.js
+++ src/Checkbox/index.stories.js
@@ -9,6 +9,15 @@ Overview.args = {
   name: "spam",
 };
 
+export const MultipleCheckboxes = () => (
+  <>
+    <h3 className="margin--bottom">Permissions</h3>
+    <Checkbox label="See statements and documents" name="view" />
+    <Checkbox label="Make deposits" name="deposit" />
+    <Checkbox label="Make withdraws" name="widthdraw" />
+  </>
+);
+
 export default {
   title: "Components/Checkbox",
   component: Checkbox,

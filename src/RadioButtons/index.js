2d97f324948ce563b5ea5af58ff69e0ebeb0d055
akdetrick <adam.detrick@narmitech.com>
2022-04-05 09:34:24 -0400

    chore: wip radio cards

--- src/RadioButtons/index.js
+++ src/RadioButtons/index.js
@@ -1,47 +1,94 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+import cc from "classcat";
 
 /*
 The Narmi RadioButtons component expects an "options" Prop, which is an object where the keys are the radiobutton
 labels and the values are the radiobutton values. An "initialvalue" Prop can be passed to set a default checked
 radiobutton.
 
-i.e.
-    <RadioButtons
-      initialValue={"true"}
-      options={{ "True": "true", "False": "false" }}
-    />
+```
+  <RadioButtons
+    initialValue={"secondValue"}
+    options={{ "First Label": "firstValue", "Second Label": "secondValue" }}
+  />
+```
 */
 
-const RadioButtons = ({ options, name, initialValue, ...containerProps }) => {
+const RadioButtons = ({
+  options,
+  name,
+  initialValue,
+  kind = "normal",
+  onChange = () => {},
+  ...containerProps
+}) => {
+  const [checkedValue, setCheckedValue] = useState(initialValue);
+  const [focusedValue, setFocusedValue] = useState(null);
+
+  const handleChange = (e) => {
+    setCheckedValue(e.target.value);
+    onChange(e);
+  };
+
+  const handleFocus = ({ target }) => {
+    setFocusedValue(target.value);
+  };
+
+  const handleBlur = () => {
+    setFocusedValue(null);
+  };
+
   return (
-    <div className="nds-radiobutton-group nds-typography" {...containerProps}>
+    <div
+      className="nds-radiobuttons nds-typography"
+      onChange={handleChange}
+      {...containerProps}
+    >
       {Object.entries(options).map(([label, value]) => (
-        <div className="nds-radiobutton-container" key={value}>
-          <label className="nds-label">
-            {label}
-            <input
-              type="radio"
-              defaultChecked={initialValue === value}
-              value={value}
-              name={name}
-            />
-            <div className="nds-checkmark"></div>
-          </label>
-        </div>
+        <label
+          className={cc([
+            "nds-radiobuttons-option",
+            `nds-radiobuttons-option--${kind}`,
+            {
+              "nds-radiobuttons-option--checked": checkedValue == value,
+              "nds-radiobuttons-option--focused": focusedValue == value,
+            },
+          ])}
+          key={value}
+        >
+          {label}
+          <input
+            type="radio"
+            checked={checkedValue === value}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            value={value}
+            name={name}
+          />
+          <div className="nds-checkmark"></div>
+        </label>
       ))}
     </div>
   );
 };
 
 RadioButtons.propTypes = {
+  /** Map of label strings to input values */
   options: PropTypes.object,
+  /** name of radiogroup */
   name: PropTypes.string,
+  /** initially selected option by input value */
   initialValue: PropTypes.any,
-};
-
-RadioButtons.defaultProps = {
-  initialValue: false,
+  /** change callback invoked with input value */
+  onChange: PropTypes.func,
+  /**
+   * `normal` - display input and label normally
+   *
+   * `card` - display input and label as a toggleable card
+   */
+  kind: PropTypes.oneOf(["normal", "card"]),
 };
 
 export default RadioButtons;

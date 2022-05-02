e1302e73813bfb6ab9d232eae1411250233e4dda
Adam <adam.undus@narmi.com>
2022-05-02 16:15:58 -0400

    lint

--- src/formatters/formatNumber.js
+++ src/formatters/formatNumber.js
@@ -42,7 +42,7 @@ const formatNumber = (input, style = "currency") => {
   }
   catch {
     // Failover for browsers that do not support Intl.NumberFormat
-    return '$' + toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
   }
 };
 

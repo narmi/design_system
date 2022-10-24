7781d2d2efc822d1ae2f29cebf664d3e122cdb8d
Adam Detrick <adam.detrick@narmitech.com>
2022-10-24 15:59:36 -0400

    feat(icons): document mobile-compatible icons

--- src/icons/Icons.stories.js
+++ src/icons/Icons.stories.js
@@ -1,20 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
+import {
+  Title,
+  Subtitle,
+  Primary,
+  Stories,
+} from '@storybook/addon-docs';
 import iconSelection from "dist/icons/selection.json";
+import compatIcons from "./2022.7-compat-icons";
 
-export default {
-  title: "Style/Icons",
-};
-
-export const Icons = () => {
-  const iconNames = [];
-
-  iconSelection.icons.forEach((iconData) => {
-    const namesAndAliases = iconData.properties.name.split(",");
-    namesAndAliases.forEach((name) => {
-      iconNames.push(name.trim());
-    });
-  });
-
+const IconDocs = ({ iconNames }) => {
   return (
     <div className="nds-typography">
       <div className="icon-demo">
@@ -27,4 +22,53 @@ export const Icons = () => {
       </div>
     </div>
   );
+};
+
+IconDocs.propTypes = {
+  iconNames: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+// Main Icons story
+export const Icons = () => {
+  const iconNames = [];
+
+  iconSelection.icons.forEach((iconData) => {
+    const namesAndAliases = iconData.properties.name.split(",");
+    namesAndAliases.forEach((name) => {
+      iconNames.push(name.trim());
+    });
+  });
+  return <IconDocs iconNames={iconNames} />;
+};
+
+// Mobile-Compatible Icons story
+export const MobileCompatibleIcons = () => (
+  <>
+    <IconDocs iconNames={compatIcons} />
+  </>
+);
+MobileCompatibleIcons.parameters = {
+  docs: {
+    description: {
+      story: `This set of icons is safe to use in our mobile application.
+      These shapes are supported down to release \`v2022.7\` to account for
+      users on older versions of the mobile app.`,
+    },
+  },
+};
+
+export default {
+  title: "Style/Icons",
+  parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Primary />
+          <Stories />
+        </>
+      ),
+    },
+  },
 };

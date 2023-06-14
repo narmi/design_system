0170558b93e19a80a04a620707c797d85c4463d2
Johnathan Weisner <johnathan@narmi.com>
2023-06-14 13:52:18 -0400

    Fix tests

--- src/IconButton/index.test.js
+++ src/IconButton/index.test.js
@@ -4,44 +4,46 @@ import IconButton from "./";
 
 const getIconButton = () => screen.getByTestId("nds-icon-button");
 
+const LABEL = "info";
+
 describe("IconButton", () => {
   it("has correct attributes for default props", () => {
-    render(<IconButton label={LABEL} />);
+    render(<IconButton label={LABEL} name="info" />);
     const iconButton = getIconButton();
-    expect(button).toBeInTheDocument();
-    expect(button).toHaveClass("nds-icon-button--plain");
+    expect(iconButton).toBeInTheDocument();
+    expect(iconButton).toHaveClass("nds-icon-button--plain");
   });
 
   it("fires click callback", () => {
     const handleClick = jest.fn();
-    render(<IconButton label={LABEL} onClick={handleClick} />);
+    render(<IconButton label={LABEL} name="info" onClick={handleClick} />);
     const iconButton = getIconButton();
     expect(handleClick).not.toHaveBeenCalled();
-    fireEvent.click(button);
+    fireEvent.click(iconButton);
     expect(handleClick).toHaveBeenCalled();
   });
 
   it("applies correct styling for `disabled`", () => {
-    render(<IconButton label={LABEL} disabled={true} />);
+    render(<IconButton label={LABEL} name="info" disabled={true} />);
     const iconButton = getIconButton();
-    expect(button).toHaveClass("nds-icon-button--disabled");
+    expect(iconButton).toHaveClass("nds-icon-button--disabled");
   });
 
   it.each(["xs", "s", "m", "l"])(
     "has expected classes for `%s` size",
     (size) => {
-      render(<IconButton label={LABEL} size={size} />);
+      render(<IconButton label={LABEL} name="info" size={size} />);
       const iconButton = getIconButton();
-      expect(button).toHaveClass(`fontSize--${size}`);
+      expect(iconButton).toHaveClass(`fontSize--${size}`);
     }
   );
 
   it.each(["plain", "primary", "secondary"])(
     "has expected classes for %s button",
     (kind) => {
-      render(<IconButton label={LABEL} kind={kind} />);
+      render(<IconButton label={LABEL} name="info" kind={kind} />);
       const iconButton = getIconButton();
-      expect(button).toHaveClass(`nds-button--${kind}`);
+      expect(iconButton).toHaveClass(`nds-icon-button--${kind}`);
     }
   );
 });

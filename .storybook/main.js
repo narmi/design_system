c734a2d49e4ae8c1b183783331236b49e8e0ce10
Adam Detrick <adam.detrick@narmitech.com>
2023-10-26 17:21:14 -0400

    docs(storybook): update storybook config to accept typescript

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,9 @@ module.exports = {
   core: {
     builder: "webpack5",
   },
+  typescript: {
+    reactDocgen: "react-docgen-typescript-plugin",
+  },
   stories: ["../**/*.stories.@(mdx|js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-essentials",

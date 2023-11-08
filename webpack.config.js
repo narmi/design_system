0f780ef6d8e54980da4f341b047b839af651915b
Adam Detrick <adam.detrick@narmitech.com>
2023-11-08 18:05:58 -0500

    chore: build works with tsc emitting d file

--- webpack.config.js
+++ webpack.config.js
@@ -52,14 +52,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(ts|tsx)$/,
-        exclude: [/node_modules/],
-        use: {
-          loader: "ts-loader",
-        },
-      },
-      {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|jsx|ts|tsx)$/,
         exclude: [/node_modules/],
         use: {
           loader: "babel-loader",

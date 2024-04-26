8493255014bce5000b5ae0be84c812d7a1d3df44
Adam Detrick <adam.detrick@narmitech.com>
2024-04-26 13:50:09 -0400

    chore: update commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,1 @@
-/**
- * @module commitlint config
- * @see https://commitlint.js.org/
- *
- * Enforces Conventional Commits specification in our commit messages.
- * commitlint is run via a husty precommit hook.
- */
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-};
+module.exports = { extends: ["@commitlint/config-conventional"] };

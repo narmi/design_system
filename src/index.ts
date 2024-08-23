47fec8cb47b00c96789a43236ea270b9e520ac05
Alexander <alex@narmi.com>
2024-08-23 11:05:59 -0700

    declare CollapsibleCard for .4 beta

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import SeparatorList from "./SeparatorList";
 import Slider from "./Slider";
 
+declare const CollapsibleCard;
+
 export * from "./types/Icon.types"
 export {
+  CollapsibleCard,
   SeparatorList,
   Slider,
 }

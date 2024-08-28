2412f09ed592afaa616bed9ffadbe08c2cff00cb
Adam Detrick <adam.detrick@narmitech.com>
2024-08-28 13:24:28 -0400

    feat(MultiSelect): mv selection indicator to right of item

--- src/MultiSelect/index.js
+++ src/MultiSelect/index.js
@@ -7,6 +7,7 @@ import cc from "classcat";
 import DropdownTrigger from "../DropdownTrigger";
 import MultiSelectItem from "./MultiSelectItem";
 import FieldToken from "../FieldToken";
+import Row from "../Row";
 
 const noop = () => {};
 
@@ -239,17 +240,20 @@ const MultiSelect = ({
                         highlightedIndex === index,
                       "rounded--top": index === 0,
                       "rounded--bottom": index === items.length - 1,
-                      "nds-select-item--hasGutter": hasSelectedItem,
                     },
                   ])}
                   {...getItemProps({ item, index })}
                   role="option"
                   aria-selected={isSelected(selectedItems, item).toString()}
                 >
-                  {isSelected(selectedItems, item) && (
-                    <span className="narmi-icon-check fontSize--l fontWeight--bold" />
-                  )}
-                  {item}
+                  <Row as="span">
+                    <Row.Item as="span">{item}</Row.Item>
+                    {isSelected(selectedItems, item) && (
+                      <Row.Item as="span" shrink>
+                        <span className="narmi-icon-check fontSize--l fontWeight--bold" />
+                      </Row.Item>
+                    )}
+                  </Row>
                 </li>
               ))}
             </ul>

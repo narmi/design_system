f5f342df1b17a22488cfdfa27611b39976a1773f
Adam Undus <adam.undus@narmi.com>
2024-09-23 10:18:38 -0400

    feat(timelineEvent): Add support for a tooltip to TimelineEvent component

--- src/TimelineEvent/index.js
+++ src/TimelineEvent/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import iconSelection from "src/icons/selection.json";
 import cc from "classcat";
+import ToolTip from "../Tooltip"
 
 export const VALID_ICON_NAMES = iconSelection.icons.map(
   (icon) => icon.properties.name
 );
 
-const TimelineEvent = ({ kind = "node", icon, imgUrl, initial, children }) => {
+const TimelineEvent = ({ kind = "node", icon, imgUrl, initial, tooltip, children }) => {
   const useInitial = !icon && !imgUrl && initial;
   return (
     <div
@@ -28,8 +29,8 @@ const TimelineEvent = ({ kind = "node", icon, imgUrl, initial, children }) => {
           ])}
           style={{ backgroundImage: imgUrl ? `url(${imgUrl})` : "none" }}
         >
-          {useInitial && <span>{initial}</span>}
-          {icon && <span className={`narmi-icon-${icon}`} />}
+          {useInitial && (tooltip ? <ToolTip text={tooltip}><span>{initial}</span></ToolTip> : <span>{initial}</span>)}
+          {icon && (tooltip ? <ToolTip text={tooltip}><span className={`narmi-icon-${icon}`} /></ToolTip> : <span className={`narmi-icon-${icon}`} />)}
         </div>
         {kind !== "start" && (
           <svg
@@ -78,6 +79,10 @@ TimelineEvent.propTypes = {
   initial: PropTypes.string,
   /**
    * Timeline event content (any JSX)
+   */
+  tooltip: PropTypes.string,
+  /**
+   * Hover tooltip content for the icon
    */
   children: PropTypes.oneOfType([
     PropTypes.node,

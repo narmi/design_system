dc9d47596a5f458fefd8c7081bdc508cc0dd4c09
Peter Yee <graphicsforge@gmail.com>
2024-09-23 09:58:35 -0400

    feat(Error): enable displaying of multiple errors

--- src/Error/index.js
+++ src/Error/index.js
@@ -1,18 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Error = ({ error, marginTop = "xxs" }) => {
-  if (!error) return null;
+const ErrorLine = ({ errorLine, marginTop = "xxs" }) => {
+  if (!errorLine) return null;
   return (
     <div className={`nds-input-error margin--top--${marginTop}`}>
       <div className="fontSize--s margin--right--xxs narmi-icon-x-circle" />
-      {error}
+      {errorLine}
     </div>
   );
 };
-Error.propTypes = {
+ErrorLine.propTypes = {
   error: PropTypes.string,
   marginTop: PropTypes.oneOf(["xxs", "xs", "s", "m", "l", "xl", "xxl", "none"])
 };
+
+const Error = ({ error, marginTop = "xxs" }) => {
+  if (!error) return null;
+  if (Array.isArray(error)) {
+    return (
+      <div className="nds-input-errorlist">
+        {error.map((errorLine, index) => (
+          <ErrorLine key={index} errorLine={errorLine} marginTop={index ? marginTop : "xxs"} />
+        ))}
+      </div>
+    );
+  }
+  return <ErrorLine errorLine={error} marginTop={marginTop} />;
+};
+Error.propTypes = {
+  error: PropTypes.oneOf([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
+  marginTop: PropTypes.oneOf(["xxs", "xs", "s", "m", "l", "xl", "xxl", "none"])
+};
+
 
 export default Error;

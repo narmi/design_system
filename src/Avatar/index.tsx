bb8537e05bcd9536ceb6105a97f6c595003849c3
Christian Szablewski-Paz <christian.szablewski-paz@narmitech.com>
2024-11-27 11:24:30 -0500

    fix comments, remove testId from signature

--- src/Avatar/index.tsx
+++ src/Avatar/index.tsx
@@ -4,15 +4,15 @@ import cc from "classcat";
 import AsElement from "../util/AsElement";
 
 export interface AvatarProps {
-  // aria-label for accessibility
+  /** aria-label for accessibility */
   label: string;
-  // Fixed height and width of the avatar. Default: "s".
+  /** Fixed height and width of the avatar. Default: "s". */
   size?: "xs" | "s" | "m";
-  // String to display in the avatar. If imgurl is provided, this will be ignored.
+  /** String to display in the avatar. If imgurl is provided, this will be ignored. */
   initials?: string;
-  // Optional: URL of the image to display in the avatar. If provided, initials will be ignored.
+  /** Optional: URL of the image to display in the avatar. If provided, initials will be ignored. */
   imgurl?: string;
-  // Optional: URL to navigate to when the avatar is clicked
+  /** Optional: URL to navigate to when the avatar is clicked */
   linkurl?: string;
 }
 
@@ -22,7 +22,6 @@ const Avatar = ({
   initials,
   imgurl,
   linkurl,
-  testId,
 }: AvatarProps) => {
   const backgroundImage = imgurl
     ? { backgroundImage: `url(${imgurl})`, backgroundSize: "cover" }
@@ -46,24 +45,6 @@ const Avatar = ({
       {initials}
     </AsElement>
   );
-};
-
-Avatar.propTypes = {
-  /**
-   * When `true`, the row item shrinks to content size of its children.
-   * Otherwise, the item will expand to fill remaining space in the row.
-   */
-  label: PropTypes.string,
-  /** The html element to render as the root node of `Row` */
-  size: PropTypes.oneOf(["xs", "s", "m", "l", "xl"]),
-  /** Optional: controls className while maintaining default nds-row-item styling if left unspecified */
-  className: PropTypes.string,
-  children: PropTypes.oneOfType([
-    PropTypes.node,
-    PropTypes.arrayOf(PropTypes.node),
-  ]),
-  /** Optional value for `data-testid` attribute */
-  testId: PropTypes.string,
 };
 
 export default Avatar;

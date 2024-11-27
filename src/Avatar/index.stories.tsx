e6e3f819022eabb30cf5a6f2cba3d59f0170a73d
Christian Szablewski-Paz <christian.szablewski-paz@narmitech.com>
2024-11-27 10:35:09 -0500

    initial commit

---
+++ src/Avatar/index.stories.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/jsx-key */
+import React, { useState } from "react";
+import Avatar from "../Avatar";
+
+// FIXME: code and story for `footerContent`
+// FIXME: code and story for render prop trigger
+
+export const Overview = () => {
+  return (
+    <div style={{ margin: "8rem" }}>
+      <Avatar initials="CP" label="Christian Paz" />
+    </div>
+  );
+};
+
+export default {
+  title: "Components/Avatar",
+  component: Avatar,
+};
---
+++ src/Avatar/index.tsx
@@ -0,0 +1,72 @@
+import React, { PropsWithChildren } from "react";
+import PropTypes from "prop-types";
+import cc from "classcat";
+import AsElement from "../util/AsElement";
+
+/**
+ * Child component of `Row`.
+ * When a `Row.Item` has a boolean prop of `shrink`, it will shrink to content width.
+ */
+const Avatar = ({
+  label = "",
+  size = "m",
+  initials = "",
+  imgurl = "",
+  linkurl = "",
+  children,
+  testId,
+}: PropsWithChildren<{
+  label?: string;
+  size?: "xs" | "s" | "m" | "l" | "xl";
+  initials?: string;
+  imgurl?: string;
+  linkurl?: string;
+  testId?: string;
+}>) => {
+  if (initials && imgurl) {
+    console.warn(
+      "Avatar component received both initials and imgurl props. Defaulting to imgurl.",
+    );
+  }
+  if (!initials && !imgurl) {
+    throw new Error(
+      "Avatar component requires either initials or imgurl prop.",
+    );
+  }
+  return (
+    <AsElement
+      elementType={linkurl ? "a" : "div"}
+      className={cc([
+        "nds-typography",
+        "nds-avatar",
+        // `nds-avatar--${size}`,
+        "alignChild--center--center",
+        "bgColor--theme--primary",
+      ])}
+      aria-label={label}
+      data-testid={testId}
+    >
+      <div className="nds-avatar">{initials}</div>
+    </AsElement>
+  );
+};
+
+Avatar.propTypes = {
+  /**
+   * When `true`, the row item shrinks to content size of its children.
+   * Otherwise, the item will expand to fill remaining space in the row.
+   */
+  label: PropTypes.string,
+  /** The html element to render as the root node of `Row` */
+  size: PropTypes.oneOf(["xs", "s", "m", "l", "xl"]),
+  /** Optional: controls className while maintaining default nds-row-item styling if left unspecified */
+  className: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]),
+  /** Optional value for `data-testid` attribute */
+  testId: PropTypes.string,
+};
+
+export default Avatar;

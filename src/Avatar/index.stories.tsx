05d8b893668aab95ebd3ed93ac04754b6525592b
Christian Szablewski-Paz <christian.szablewski-paz@narmitech.com>
2024-11-27 11:39:08 -0500

    add examples

--- src/Avatar/index.stories.tsx
+++ src/Avatar/index.stories.tsx
@@ -1,14 +1,40 @@
 /* eslint-disable react/jsx-key */
 import React from "react";
 import Avatar from "../Avatar";
-
-const Template = (args) => <Avatar {...args} />;
+import Row from "../Row";
 
 export const Overview = () => {
+  return <Avatar initials="CP" label="Christian Paz" />;
+};
+
+export const WithImage = () => {
   return (
-    <div style={{ margin: "8rem" }}>
-      <Avatar initials="CP" label="Christian Paz" />
-    </div>
+    <Avatar
+      imgurl="https://media.licdn.com/dms/image/v2/D4E0BAQHnVTkZjhvlQg/company-logo_200_200/company-logo_200_200/0/1680295112021?e=2147483647&v=beta&t=-4A2TRSHuDiBT_anhoqTULvjXfzVh7_vZApmdXUVzsc"
+      label="Narmi linked in logo"
+    />
+  );
+};
+
+export const WithLink = () => {
+  return (
+    <Avatar initials="CP" label="Christian Paz" linkurl="https://narmi.com" />
+  );
+};
+
+export const Sizes = () => {
+  return (
+    <Row>
+      <Row.Item>
+        <Avatar initials="xs" label="extra small" size="xs" />
+      </Row.Item>
+      <Row.Item>
+        <Avatar initials="s" label="small" size="s" />
+      </Row.Item>
+      <Row.Item>
+        <Avatar initials="m" label="medium" size="m" />
+      </Row.Item>
+    </Row>
   );
 };
 

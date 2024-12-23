7aba573ef153bbeea5ab42e587186ddc28053a9e
Adam Detrick <adam.detrick@narmitech.com>
2024-12-23 16:11:59 -0500

    chore: change engines to LTS node

--- src/Toggle/index.test.js
+++ src/Toggle/index.test.js
@@ -8,11 +8,6 @@ describe("Toggle", () => {
     expect(screen.getByRole("switch")).toBeInTheDocument();
   });
 
-  it("renders as active when `defaultActive` prop is true", () => {
-    render(<Toggle defaultActive={true} />);
-    expect(screen.getByRole("switch")).toHaveAttribute("aria-checked", "true");
-  });
-
   it("fires change callback with expected state", () => {
     const handleChange = jest.fn();
     render(<Toggle onChange={handleChange} />);

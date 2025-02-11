8c6fcfca9258c12d5585af6f5b808dc00f48bb14
Christian Szablewski-Paz <christian.szablewski-paz@narmitech.com>
2025-02-11 10:27:47 -0500

    chore(MenuButton): rm unintended whitespace

--- src/MenuButton/index.js
+++ src/MenuButton/index.js
@@ -170,7 +170,6 @@ const MenuButton = ({
                           />
                         </Row.Item>
                       )}
-
                     </Row>
                   </MenuItem>
                 );

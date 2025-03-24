c78563e7ca569e2314745d804f9c53a4b2a8a55f
Adam Detrick <adam.detrick@narmitech.com>
2025-03-24 15:50:48 -0400

    feat(ProgressBar): update component to match new design sizes and styles

--- src/ProgressBar/index.js
+++ src/ProgressBar/index.js
@@ -1,21 +1,61 @@
-import React from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
 
-const ProgressBar = ({ totalWidth = "100%", percentComplete = 0 }) => {
+// sizes:
+// 4px
+// 8px
+
+// variants:
+// - flat, full bleed w/o rounding
+
+const ProgressBar = ({
+  totalWidth = "100%",
+  percentComplete = 0,
+  dur = 0.25,
+}) => {
+  const [currentPct, setCurrentPct] = useState(0);
+  const lineRef = useRef(null);
+  const animateRef = useRef(null);
+
+  useLayoutEffect(() => {
+    if (lineRef.current) {
+      if (animateRef.current) {
+        // TODO: when `to` is 0, `from` must also be set to 0
+        animateRef.current.setAttribute("from", `${currentPct}%`);
+        animateRef.current.setAttribute("to", `${percentComplete}%`);
+        animateRef.current.beginElement();
+        setCurrentPct(percentComplete);
+      }
+    }
+  }, [percentComplete]);
+
   return (
-    <div
-      className="nds-progressbar"
-      style={{ width: `${totalWidth}`, height: "var(--font-size-default)" }}
-    >
-      <svg xmlns="http://www.w3.org/2000/svg">
-        <line
-          x1="0%"
-          y1="50%"
-          x2={`${percentComplete}%`}
-          y2="50%"
-          strokeWidth="100%"
-        />
-      </svg>
+    <div className="nds-progressbar" style={{ width: `${totalWidth}` }}>
+      {percentComplete > 0 && (
+        <svg xmlns="http://www.w3.org/2000/svg">
+          <line
+            ref={lineRef}
+            x1="0%"
+            y1="50%"
+            x2={`${currentPct}%`}
+            y2="50%"
+            strokeLinecap="round"
+          >
+            <animate
+              ref={animateRef}
+              attributeName="x2"
+              from="0%"
+              to="0%"
+              dur={`${dur}s`}
+              fill="freeze"
+              restart="always"
+              calcMode="spline"
+              keyTimes="0;1"
+              keySplines="0 0 0.5 1" // <https://cubic-bezier.com/#0,0,.5,1>
+            />
+          </line>
+        </svg>
+      )}
     </div>
   );
 };
--- src/ProgressBar/index.stories.js
+++ src/ProgressBar/index.stories.js
@@ -1,11 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import ProgressBar from "./";
+import Row from "../Row";
+import Button from "../Button";
+import TextInput from "../TextInput";
 
 const Template = (args) => <ProgressBar {...args} />;
 
 export const Overview = Template.bind({});
 Overview.args = {
   percentComplete: 25,
+};
+
+export const Animation = () => {
+  const [pct, setPct] = useState(0);
+  const [dur, setDur] = useState(undefined);
+
+  const handleChange = (amount) => {
+    if (amount >= 0 && amount <= 100) {
+      setPct((p) => p + amount);
+    }
+  };
+
+  return (
+    <>
+      <div className="margin--bottom">
+        <ProgressBar percentComplete={pct} dur={dur} />
+      </div>
+      <Row>
+        <Row.Item shrink>
+          <Button size="xs" label="Add 2%" onClick={() => handleChange(2)} />
+        </Row.Item>
+        <Row.Item shrink>
+          <Button size="xs" label="Add 10%" onClick={() => handleChange(10)} />
+        </Row.Item>
+        <Row.Item shrink>
+          <Button size="xs" label="Add 25%" onClick={() => handleChange(25)} />
+        </Row.Item>
+        <Row.Item shrink>
+          <Button size="xs" label="Add 33%" onClick={() => handleChange(33)} />
+        </Row.Item>
+        <Row.Item shrink>
+          <Button
+            kind="secondary"
+            size="xs"
+            label="Reset"
+            onClick={() => setPct(0)}
+          />
+        </Row.Item>
+      </Row>
+      <div className="margin--top">
+        <div>
+          <label htmlFor="dur">Duration (in seconds)</label>
+        </div>
+        <input
+          type="number"
+          step="0.05"
+          id="dur"
+          min="0.1"
+          max="1"
+          value={dur}
+          onChange={(e) => {
+            setPct(0);
+            setDur(e.target.value);
+          }}
+        />
+      </div>
+    </>
+  );
 };
 
 export default {

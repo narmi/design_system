4521029027cb497c4694d3f3adc83961edd13b4c
Adam Detrick <adam.detrick@narmitech.com>
2025-04-04 15:44:16 -0400

    chore: lint

--- src/Combobox/index.js
+++ src/Combobox/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import cc from "classcat";
 import iconSelection from "src/icons/selection.json";

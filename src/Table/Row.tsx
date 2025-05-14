cff04fa16668fa2d0d4f68460fac7b97a1edaabb
Adam Detrick <adam.detrick@narmitech.com>
2025-05-14 16:10:55 -0400

    chore: set up layout rule on row

--- src/Table/Row.tsx
+++ src/Table/Row.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable jsx-a11y/interactive-supports-focus */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React from "react";
+import React, { useContext } from "react";
 import type { CellProps } from "./Cell";
+import TableLayoutContext from "./util/tableLayoutContext";
 
 const noop = () => {};
 
@@ -10,23 +11,30 @@ interface TableRowProps {
   onRowClick: () => void;
 }
 
-const Row = ({ children, onRowClick = noop }: TableRowProps) => (
-  <div role="row" onClick={onRowClick}>
-    {/**
-     * Render children in the order `Row` received them, but inject a
-     * `_colIndex` prop so `Cell` can access its visibilty configuration.
-     */}
-    {React.Children.toArray(children).map((child, i) => {
-      if (React.isValidElement(child)) {
-        return React.cloneElement(child as React.ReactElement<CellProps>, {
-          key: i,
-          _colIndex: i,
-        });
-      }
-      console.warn("Table.Row ecountered an unknown child");
-      return child;
-    })}
-  </div>
-);
+const Row = ({ children, onRowClick = noop }: TableRowProps) => {
+  const { currentBreakpoint, colLayout } = useContext(TableLayoutContext);
+  return (
+    <div
+      role="row"
+      onClick={onRowClick}
+      style={{ gridTemplateColumns: colLayout[currentBreakpoint] }}
+    >
+      {/**
+       * Render children in the order `Row` received them, but inject a
+       * `_colIndex` prop so `Cell` can access its visibilty configuration.
+       */}
+      {React.Children.toArray(children).map((child, i) => {
+        if (React.isValidElement(child)) {
+          return React.cloneElement(child as React.ReactElement<CellProps>, {
+            key: i,
+            _colIndex: i,
+          });
+        }
+        console.warn("Table.Row ecountered an unknown child");
+        return child;
+      })}
+    </div>
+  );
+};
 
 export default Row;

95df3bb51310f0eead64fb06621d73dc004f4e17
Adam Detrick <adam.detrick@narmitech.com>
2025-10-10 18:16:32 -0400

    refactor(PlainButton)!: remove PlainButton component and classes

--- src/index.js
+++ src/index.js
@@ -25,7 +25,6 @@ import LoadingSkeleton from "./LoadingSkeleton";
 import MenuButton from "./MenuButton";
 import MultiSelect from "./MultiSelect";
 import Pagination from "./Pagination";
-import PlainButton from "./PlainButton";
 import ProgressBar from "./ProgressBar";
 import Popover from "./Popover";
 import Radio from "./Radio";
@@ -85,7 +84,6 @@ export {
   MenuButton,
   MultiSelect,
   Pagination,
-  PlainButton,
   ProgressBar,
   Popover,
   Radio,

00f06e53b6c30b387913a39156551a55d95ebec4
akdetrick <adam.detrick@narmitech.com>
2025-12-07 21:11:35 +0000

    Deploying to gh-pages from @ narmi/design_system@120d2283e0a5990fcdbabb332c3d033fbbcccb28 🚀

---
+++ assets/index-BVArFuVr.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-VD1YF__z.js";import{r as o}from"./index-Chjiymov.js";var p=o();const t=r(p);export{t as P};
--- assets/list-BwnLh-Wp.js
+++
@@ -1,4 +0,0 @@
-import{j as t,M as r,C as i}from"./blocks-wX8QGgE2.js";import{useMDXComponents as n}from"./index-DepDrwJh.js";import"./ClassExample-CwBCagLN.js";import{L as m,a}from"./list.stories-BWdK7_Nr.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-Ci3LgRk2.js";import"./index-CzRXvaZI.js";import"./index-CGe1zHLw.js";import"./index-xE5G1eq7.js";import"./AsElement-D4IW6aGF.js";import"./index-RvS1aOr8.js";function e(s){const o={h1:"h1",p:"p",...n(),...s.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:m}),`
-`,t.jsx(o.h1,{id:"list-reset",children:"List reset"}),`
-`,t.jsx(o.p,{children:"Resets browser defaults on lists"}),`
-`,t.jsx(i,{of:a})]})}function L(s={}){const{wrapper:o}={...n(),...s.components};return o?t.jsx(o,{...s,children:t.jsx(e,{...s})}):e(s)}export{L as default};

2a58ede051ab6ac23ad71d03e044d0f8b254fcfc
akdetrick <adam.detrick@narmitech.com>
2026-02-26 20:39:32 +0000

    Deploying to gh-pages from @ narmi/design_system@7d1c5779d9f6bdcc9402fe97c0f2571e334c7354 🚀

--- assets/layout-Dqs5lZ7c.js
+++
@@ -1,4 +0,0 @@
-import{j as t,M as i,C as r}from"./blocks-kDMRcVtf.js";import{useMDXComponents as s}from"./index-CLThjrOI.js";import{L as a,S as m}from"./layout.stories-tYzcgp98.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-voVwBnfo.js";import"./index-DFYBChvh.js";import"./index-Bz-otBT1.js";import"./TokenTable-BSIAN82P.js";import"./index-RvS1aOr8.js";function e(n){const o={h1:"h1",h2:"h2",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Design Tokens/Layout",of:a}),`
-`,t.jsx(o.h1,{id:"layout-tokens",children:"Layout Tokens"}),`
-`,t.jsx(o.h2,{id:"spacing",children:"Spacing"}),`
-`,t.jsx(r,{of:m,style:{height:"auto"}})]})}function g(n={}){const{wrapper:o}={...s(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{g as default};
--- assets/useFetch-DEr9C-KU.js
+++
@@ -1,1 +0,0 @@
-import"./iframe-voVwBnfo.js";

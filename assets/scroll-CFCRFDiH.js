ae329213136a80888fed0defe1979f9983f711ac
akdetrick <adam.detrick@narmitech.com>
2026-03-25 01:17:22 +0000

    Deploying to gh-pages from @ narmi/design_system@adb74a850e91ff2bd5d0bd8ca592509cc76881ad 🚀

--- assets/scroll-CFCRFDiH.js
+++
@@ -1,6 +0,0 @@
-import{j as o,M as l,C as i}from"./blocks-B7HrsM7h.js";import{useMDXComponents as t}from"./index-CbpOldUB.js";import"./ClassExample-BGj4Qspb.js";import{S as s,C as c}from"./scroll.stories-DmPI6yEw.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-CnYG4fmz.js";import"./index-CzRAJZ48.js";import"./index-DqmJRNLb.js";import"./index-YU3J6xuU.js";import"./AsElement-BTAGnGvi.js";import"./index-RvS1aOr8.js";function e(r){const n={code:"code",h1:"h1",p:"p",...t(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(l,{of:s}),`
-`,o.jsx(n.h1,{id:"controlling-overflow",children:"Controlling Overflow"}),`
-`,o.jsxs(n.p,{children:["Disable scrolling with ",o.jsx(n.code,{children:"overflow--hidden"}),`.
-Enable scrolling with `,o.jsx(n.code,{children:"overflow--scroll"}),"."]}),`
-`,o.jsx(n.p,{children:"Useful for locking body scroll when modals are open."}),`
-`,o.jsx(i,{of:c})]})}function g(r={}){const{wrapper:n}={...t(),...r.components};return n?o.jsx(n,{...r,children:o.jsx(e,{...r})}):e(r)}export{g as default};

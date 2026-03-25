29d4e8a209477f2cc837fb23de3200d03200bbba
akdetrick <adam.detrick@narmitech.com>
2026-03-25 01:33:07 +0000

    Deploying to gh-pages from @ narmi/design_system@8b698f82e27578705343716fafa8860171d8daac 🚀

---
+++ assets/font-BpcDPIWa.js
@@ -0,0 +1,12 @@
+import{j as t,M as h,C as e}from"./blocks-B7HrsM7h.js";import{useMDXComponents as s}from"./index-CbpOldUB.js";import"./TokenTable-CPDbI5kV.js";import{Color as r,Size as l,Family as m,Weight as a,LineHeight as x}from"./font.stories-CaC7s3iY.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-CnYG4fmz.js";import"./index-CzRAJZ48.js";import"./index-DqmJRNLb.js";import"./index-RvS1aOr8.js";function i(o){const n={h1:"h1",h2:"h2",...s(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(h,{title:"Design Tokens/Font"}),`
+`,t.jsx(n.h1,{id:"font-tokens",children:"Font Tokens"}),`
+`,t.jsx(n.h2,{id:"font-color",children:"Font Color"}),`
+`,t.jsx(e,{of:r,style:{height:"auto"}}),`
+`,t.jsx(n.h2,{id:"font-size",children:"Font Size"}),`
+`,t.jsx(e,{of:l,style:{height:"auto"}}),`
+`,t.jsx(n.h2,{id:"font-family",children:"Font Family"}),`
+`,t.jsx(e,{of:m,style:{height:"auto"}}),`
+`,t.jsx(n.h2,{id:"font-weight",children:"Font Weight"}),`
+`,t.jsx(e,{of:a,style:{height:"auto"}}),`
+`,t.jsx(n.h2,{id:"font-line-height",children:"Font Line Height"}),`
+`,t.jsx(e,{of:x,style:{height:"auto"}})]})}function C(o={}){const{wrapper:n}={...s(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(i,{...o})}):i(o)}export{C as default};

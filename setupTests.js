0e82d9bb311c19e0fb9e78f967ba93ba93e000f0
Adam Detrick <adam.detrick@narmitech.com>
2026-04-06 21:15:28 -0400

    test(Dialog): update tests for new design spec

--- setupTests.js
+++ setupTests.js
@@ -9,3 +9,17 @@ class MockResizeObserver {
 window.ResizeObserver = MockResizeObserver;
 
 window.HTMLElement.prototype.scroll = function () {};
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  }),
+});

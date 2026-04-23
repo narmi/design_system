873c38c467132326da6d674c1ca18e17089d5ddc
akdetrick <adam.detrick@narmitech.com>
2026-04-23 19:32:20 +0000

    Deploying to gh-pages from @ narmi/design_system@197a76bb1e846c11035dd252bf4125e2e92bbb59 🚀

--- assets/layout-C1UMhuvC.js
+++
@@ -1,4 +0,0 @@
-import{j as t,M as i,C as r}from"./blocks-C-WEbRbD.js";import{useMDXComponents as s}from"./index-B7rYMxK4.js";import{L as a,S as m}from"./layout.stories-BFV2qxfT.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-B57ZC_Ss.js";import"./index-BnwOPSvS.js";import"./index-a3Qu1-yF.js";import"./TokenTable-DntrV8a0.js";import"./index-RvS1aOr8.js";function e(n){const o={h1:"h1",h2:"h2",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Design Tokens/Layout",of:a}),`
-`,t.jsx(o.h1,{id:"layout-tokens",children:"Layout Tokens"}),`
-`,t.jsx(o.h2,{id:"spacing",children:"Spacing"}),`
-`,t.jsx(r,{of:m,style:{height:"auto"}})]})}function g(n={}){const{wrapper:o}={...s(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{g as default};

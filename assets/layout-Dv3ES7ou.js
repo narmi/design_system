a9f32ea72ad369a7119845c701810455bf571873
akdetrick <adam.detrick@narmitech.com>
2026-05-15 16:07:42 +0000

    Deploying to gh-pages from @ narmi/design_system@0fd1eb5317c98e43ecfe1f864f4843cef74bd5d6 🚀

---
+++ assets/layout-Dv3ES7ou.js
@@ -0,0 +1,4 @@
+import{j as t,M as i,C as r}from"./blocks-BHuQw6vC.js";import{useMDXComponents as s}from"./index-CiYTwd6u.js";import{L as a,S as m}from"./layout.stories-BCywI9vK.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-KmDyQMML.js";import"./index-5DEziAgS.js";import"./index-DegJOqdw.js";import"./TokenTable-DvV1dX0z.js";import"./index-RvS1aOr8.js";function e(n){const o={h1:"h1",h2:"h2",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Design Tokens/Layout",of:a}),`
+`,t.jsx(o.h1,{id:"layout-tokens",children:"Layout Tokens"}),`
+`,t.jsx(o.h2,{id:"spacing",children:"Spacing"}),`
+`,t.jsx(r,{of:m,style:{height:"auto"}})]})}function g(n={}){const{wrapper:o}={...s(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{g as default};
---
+++ assets/list-D3Gb4ZBE.js
@@ -0,0 +1,4 @@
+import{j as t,M as r,C as i}from"./blocks-BHuQw6vC.js";import{useMDXComponents as n}from"./index-CiYTwd6u.js";import"./ClassExample-C3De_7lW.js";import{L as m,a}from"./list.stories-DX7Wfe9D.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-KmDyQMML.js";import"./index-5DEziAgS.js";import"./index-DegJOqdw.js";import"./index-BHG-jEW9.js";import"./AsElement-sJbfcDz6.js";import"./index-RvS1aOr8.js";function e(s){const o={h1:"h1",p:"p",...n(),...s.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:m}),`
+`,t.jsx(o.h1,{id:"list-reset",children:"List reset"}),`
+`,t.jsx(o.p,{children:"Resets browser defaults on lists"}),`
+`,t.jsx(i,{of:a})]})}function L(s={}){const{wrapper:o}={...n(),...s.components};return o?t.jsx(o,{...s,children:t.jsx(e,{...s})}):e(s)}export{L as default};

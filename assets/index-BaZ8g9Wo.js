f942b069393dc12264d8ef981a69e9756fd0407f
akdetrick <adam.detrick@narmitech.com>
2026-05-28 19:03:33 +0000

    Deploying to gh-pages from @ narmi/design_system@ffb96f11269dec97376e83607e05aa3c511b4fa5 🚀

--- assets/index-BaZ8g9Wo.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./iframe-D0X-AFll.js";const d=o=>{e.useEffect(()=>(o&&document.body.classList.add("overflow--hidden"),()=>{document.body.classList.remove("overflow--hidden")}),[o])};export{d as u};
--- assets/list-pfxARnz4.js
+++
@@ -1,4 +0,0 @@
-import{j as t,M as r,C as i}from"./blocks-DHNb6eFN.js";import{useMDXComponents as n}from"./index-tl4f3qaE.js";import"./ClassExample-DYelEloz.js";import{L as m,a}from"./list.stories-D--rMg8m.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-D0X-AFll.js";import"./index-ocu3BuZC.js";import"./index-B2UpcQII.js";import"./index-C8N6sq9t.js";import"./AsElement-CqeSJfYv.js";import"./index-RvS1aOr8.js";function e(s){const o={h1:"h1",p:"p",...n(),...s.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:m}),`
-`,t.jsx(o.h1,{id:"list-reset",children:"List reset"}),`
-`,t.jsx(o.p,{children:"Resets browser defaults on lists"}),`
-`,t.jsx(i,{of:a})]})}function L(s={}){const{wrapper:o}={...n(),...s.components};return o?t.jsx(o,{...s,children:t.jsx(e,{...s})}):e(s)}export{L as default};

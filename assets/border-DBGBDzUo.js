0101ae8e05a35d7f402e71e1b0a604f09377dfe9
akdetrick <adam.detrick@narmitech.com>
2026-06-09 14:03:51 +0000

    Deploying to gh-pages from @ narmi/design_system@7b6256e490ed5f7be39adce58a768f73ff2a2335 🚀

---
+++ assets/border-DBGBDzUo.js
@@ -0,0 +1,10 @@
+import{j as e,M as i,C as n}from"./blocks-Cx5J9j1G.js";import{useMDXComponents as s}from"./index-CoO4vcxu.js";import"./ClassExample-CrWPsPK8.js";import{B as c,a as t,b as l}from"./border.stories-HfxWsi9g.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-C_MbDvQ-.js";import"./index--mrFbW_J.js";import"./index-LBnl0uug.js";import"./index-BQAF3k6x.js";import"./AsElement--wt2VBVh.js";import"./index-RvS1aOr8.js";function d(r){const o={code:"code",h1:"h1",p:"p",...s(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(i,{of:c}),`
+`,e.jsx(o.h1,{id:"border",children:"Border"}),`
+`,e.jsx(o.p,{children:"Adds a border to a given side of an element."}),`
+`,e.jsxs(o.p,{children:["Valid sides are (",e.jsx(o.code,{children:"top"}),", ",e.jsx(o.code,{children:"right"}),", ",e.jsx(o.code,{children:"bottom"}),", ",e.jsx(o.code,{children:"left"}),", ",e.jsx(o.code,{children:"all"}),", ",e.jsx(o.code,{children:"none"}),")"]}),`
+`,e.jsx(n,{of:t}),`
+`,e.jsx(o.h1,{id:"border-radius",children:"Border Radius"}),`
+`,e.jsx(o.p,{children:"Rounds the corners of the given side of an element."}),`
+`,e.jsxs(o.p,{children:["Valid sides are (",e.jsx(o.code,{children:"top"}),", ",e.jsx(o.code,{children:"right"}),", ",e.jsx(o.code,{children:"bottom"}),", ",e.jsx(o.code,{children:"left"}),", ",e.jsx(o.code,{children:"all"}),", ",e.jsx(o.code,{children:"none"}),")"]}),`
+`,e.jsxs(o.p,{children:["Valid amounts are (",e.jsx(o.code,{children:"s"}),", ",e.jsx(o.code,{children:"m"}),", ",e.jsx(o.code,{children:"l"}),")"]}),`
+`,e.jsx(n,{of:l})]})}function g(r={}){const{wrapper:o}={...s(),...r.components};return o?e.jsx(o,{...r,children:e.jsx(d,{...r})}):d(r)}export{g as default};
--- assets/scroll-CuXXtuK7.js
+++
@@ -1,6 +0,0 @@
-import{j as o,M as l,C as i}from"./blocks-D7Zsl4UE.js";import{useMDXComponents as t}from"./index-9_FqGC4q.js";import"./ClassExample-C8OjI57Q.js";import{S as s,C as c}from"./scroll.stories-4OGVyHkI.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-yxA7tmbd.js";import"./index-UkzU2qmD.js";import"./index-mOE34WOo.js";import"./index-C73OCvZ_.js";import"./AsElement-CrGRH2O-.js";import"./index-RvS1aOr8.js";function e(r){const n={code:"code",h1:"h1",p:"p",...t(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(l,{of:s}),`
-`,o.jsx(n.h1,{id:"controlling-overflow",children:"Controlling Overflow"}),`
-`,o.jsxs(n.p,{children:["Disable scrolling with ",o.jsx(n.code,{children:"overflow--hidden"}),`.
-Enable scrolling with `,o.jsx(n.code,{children:"overflow--scroll"}),"."]}),`
-`,o.jsx(n.p,{children:"Useful for locking body scroll when modals are open."}),`
-`,o.jsx(i,{of:c})]})}function g(r={}){const{wrapper:n}={...t(),...r.components};return n?o.jsx(n,{...r,children:o.jsx(e,{...r})}):e(r)}export{g as default};

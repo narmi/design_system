6f1bf2519c65c072a25e2f5815001fdd07230f83
akdetrick <adam.detrick@narmitech.com>
2026-06-10 20:28:46 +0000

    Deploying to gh-pages from @ narmi/design_system@53fab4bee460d3bcfea2699bf9b1ac56fcb00676 🚀

---
+++ assets/list-CRMbFln8.js
@@ -0,0 +1,4 @@
+import{j as t,M as r,C as i}from"./blocks-B934Pj3o.js";import{useMDXComponents as n}from"./index-B3OmdWPK.js";import"./ClassExample-ETAsAHlr.js";import{L as m,a}from"./list.stories-BodGlnNg.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-DzU1GE0T.js";import"./index-CqNDTCL5.js";import"./index-laCBnapm.js";import"./index-DpYRNHTO.js";import"./AsElement-CZK5xIX7.js";import"./index-RvS1aOr8.js";function e(s){const o={h1:"h1",p:"p",...n(),...s.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{of:m}),`
+`,t.jsx(o.h1,{id:"list-reset",children:"List reset"}),`
+`,t.jsx(o.p,{children:"Resets browser defaults on lists"}),`
+`,t.jsx(i,{of:a})]})}function L(s={}){const{wrapper:o}={...n(),...s.components};return o?t.jsx(o,{...s,children:t.jsx(e,{...s})}):e(s)}export{L as default};

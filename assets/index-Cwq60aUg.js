36304f0d3a16c34ae76647cc20fb0e89df2d4e3e
akdetrick <adam.detrick@narmitech.com>
2026-06-10 20:05:46 +0000

    Deploying to gh-pages from @ narmi/design_system@d7b7ac696a73c54ed9b03caa2c5bd8321bf97143 🚀

--- assets/index-Cwq60aUg.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-1EjhyCyX.js";import{r as o}from"./index-2tjbhunb.js";var t=o();const m=r(t);export{m as R,t as r};
---
+++ assets/scroll-ZrttKg8B.js
@@ -0,0 +1,6 @@
+import{j as o,M as l,C as i}from"./blocks-DS0Bf5I7.js";import{useMDXComponents as t}from"./index-Dghe6ca0.js";import"./ClassExample-OYgKr_Fy.js";import{S as s,C as c}from"./scroll.stories-BTDA31u1.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-Cvs2269z.js";import"./index-mxXcKhzk.js";import"./index-DWrpma1N.js";import"./index-COc-ZqqR.js";import"./AsElement-Dpa2N6RT.js";import"./index-RvS1aOr8.js";function e(r){const n={code:"code",h1:"h1",p:"p",...t(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(l,{of:s}),`
+`,o.jsx(n.h1,{id:"controlling-overflow",children:"Controlling Overflow"}),`
+`,o.jsxs(n.p,{children:["Disable scrolling with ",o.jsx(n.code,{children:"overflow--hidden"}),`.
+Enable scrolling with `,o.jsx(n.code,{children:"overflow--scroll"}),"."]}),`
+`,o.jsx(n.p,{children:"Useful for locking body scroll when modals are open."}),`
+`,o.jsx(i,{of:c})]})}function g(r={}){const{wrapper:n}={...t(),...r.components};return n?o.jsx(n,{...r,children:o.jsx(e,{...r})}):e(r)}export{g as default};

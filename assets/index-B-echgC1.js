5822b268e486253c5a52e36d8636e3cea854bf11
akdetrick <adam.detrick@narmitech.com>
2026-06-15 23:42:07 +0000

    Deploying to gh-pages from @ narmi/design_system@b542cbf9f3bab15916d2a293d2832feeaf22dca2 🚀

--- assets/index-B-echgC1.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-DLQfBhti.js";import{r as o}from"./index-35S7HnGr.js";var t=o();const m=r(t);export{m as R,t as r};
--- assets/layout-zDH0iew1.js
+++
@@ -1,4 +0,0 @@
-import{j as t,M as i,C as r}from"./blocks-CNgvnwQB.js";import{useMDXComponents as s}from"./index-BkbRiuO-.js";import{L as a,S as m}from"./layout.stories-CRcVjZwv.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-DLQfBhti.js";import"./index-B-echgC1.js";import"./index-35S7HnGr.js";import"./TokenTable-DxMUWYOM.js";import"./index-RvS1aOr8.js";function e(n){const o={h1:"h1",h2:"h2",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Design Tokens/Layout",of:a}),`
-`,t.jsx(o.h1,{id:"layout-tokens",children:"Layout Tokens"}),`
-`,t.jsx(o.h2,{id:"spacing",children:"Spacing"}),`
-`,t.jsx(r,{of:m,style:{height:"auto"}})]})}function g(n={}){const{wrapper:o}={...s(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{g as default};

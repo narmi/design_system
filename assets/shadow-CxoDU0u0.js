4a5feaa3f753899878810b2ef7ddec0d2cbca991
akdetrick <adam.detrick@narmitech.com>
2026-06-15 23:41:13 +0000

    Deploying to gh-pages from @ narmi/design_system@45bd38ece42c6d625c87a1e717067c235264b4b8 🚀

--- assets/shadow-CxoDU0u0.js
+++
@@ -1,3 +0,0 @@
-import{j as t,M as i,C as r}from"./blocks-Cy7vDt1h.js";import{useMDXComponents as s}from"./index-BqEbCmtL.js";import{S as a,E as m}from"./shadow.stories-DwjEqvzB.js";import"./preload-helper-PPVm8Dsz.js";import"./iframe-DC9YD7X8.js";import"./index-iAQrrA0j.js";import"./index-ZdqFTIo0.js";import"./TokenTable-DP9DZ9gB.js";import"./index-RvS1aOr8.js";function e(o){const n={h2:"h2",...s(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Design Tokens/Shadow",of:a}),`
-`,t.jsx(n.h2,{id:"elevation",children:"Elevation"}),`
-`,t.jsx(r,{of:m,style:{height:"auto"}})]})}function M(o={}){const{wrapper:n}={...s(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e(o)}export{M as default};

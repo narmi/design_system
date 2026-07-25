471b02c94ff1c0979d0847fb3d7989b194aa3659
Will Brand <will@narmitech.com>
2026-07-25 12:39:36 -0400

    chore(ts): convert ComboboxCategory to TypeScript

--- src/Combobox/ComboboxCategory.tsx
+++ src/Combobox/ComboboxCategory.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const ComboboxCategory = ({ children }) => <>{children}</>;
+interface ComboboxCategoryProps {
+  label: string;
+  children?: React.ReactNode;
+}
+
+const ComboboxCategory = ({ children }: ComboboxCategoryProps) => <>{children}</>;
 
 ComboboxCategory.displayName = "Combobox.Category";
 

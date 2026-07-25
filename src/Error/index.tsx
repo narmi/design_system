cb302a07d7bc6ffcdbb7aab75a59d03ceb8d7cc9
Will Brand <will@narmitech.com>
2026-07-25 12:41:10 -0400

    chore(ts): convert Error to TypeScript

--- src/Error/index.tsx
+++ src/Error/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const ErrorLine = ({ errorLine, marginTop = "xxs" }) => {
+type MarginTop = "xxs" | "xs" | "s" | "m" | "l" | "xl" | "xxl" | "none";
+
+interface ErrorLineProps {
+  errorLine?: string;
+  marginTop?: MarginTop;
+}
+
+const ErrorLine = ({ errorLine, marginTop = "xxs" }: ErrorLineProps) => {
   if (!errorLine) return null;
   return (
     <div className={`nds-input-error margin--top--${marginTop}`}>
@@ -15,7 +22,12 @@ ErrorLine.propTypes = {
   marginTop: PropTypes.oneOf(["xxs", "xs", "s", "m", "l", "xl", "xxl", "none"]),
 };
 
-const Error = ({ error, marginTop = "xxs" }) => {
+interface ErrorProps {
+  error?: string | string[];
+  marginTop?: MarginTop;
+}
+
+const Error = ({ error, marginTop = "xxs" }: ErrorProps) => {
   if (!error) return null;
   if (Array.isArray(error)) {
     return (

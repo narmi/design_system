dd8e418e8103348de6fe9707fb21038bc9c6be08
Will Brand <will@narmitech.com>
2026-07-29 12:37:51 -0600

    chore(ts): convert Select/Combobox/MultiSelect item components (#2181)

--- src/Combobox/ComboboxItem.tsx
+++ src/Combobox/ComboboxItem.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const ComboboxItem = ({ children, value }) => <>{children || value}</>;
+export interface ComboboxItemProps {
+  /**
+   * String representation of the option.
+   *
+   * This value is also used as a typeahead; if a user types "n" while
+   * the Select is open, highlight will move to the first item with a
+   * value starting with `n`.
+   */
+  value: string;
+  /**
+   * Custom typeahead string. By default, typeahead uses `value`.
+   * Use this prop to pass in a custom string you'd like the user to search
+   * against when using typeahead.
+   */
+  searchValue?: string;
+  children?: React.ReactNode;
+}
+
+const ComboboxItem = ({ children, value }: ComboboxItemProps) => (
+  <>{children || value}</>
+);
 
 ComboboxItem.displayName = "Combobox.Item";
 
@@ -19,7 +39,7 @@ ComboboxItem.propTypes = {
    * Use this prop to pass in a custom string you'd like the user to search
    * against when using typeahead.
    */
-  searchValue: PropTypes.string.isRequired,
+  searchValue: PropTypes.string,
   children: PropTypes.oneOfType([
     PropTypes.node,
     PropTypes.arrayOf(PropTypes.node),

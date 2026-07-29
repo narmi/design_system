a2b2992188d4143073001d7bdea39344bf5b9b7c
wrbrand <will.brand@gmail.com>
2026-07-29 22:48:50 +0000

    Deploying to gh-pages from @ narmi/design_system@6de0f2fc454c733209c2cfebfc38473f3c4a482c 🚀

--- assets/border-B6_ieEPd.js
+++
@@ -1,8 +0,0 @@
-import{o as e}from"./preload-helper-CHxnduP2.js";import{f as t,n,s as r,w as i}from"./blocks-D7o89Cd2.js";import{s as a}from"./chunk-LITCR56V-CZnCZIqS.js";import{t as o}from"./mdx-react-shim-BelAooES.js";import{BorderColor as s,BorderRadius as c,BorderSize as l,n as u,t as d}from"./border.stories-7wVr70t9.js";function f(e){let t={h1:`h1`,h2:`h2`,...i(),...e.components};return(0,m.jsxs)(m.Fragment,{children:[(0,m.jsx)(r,{title:`Design Tokens/Border`,of:d}),`
-`,(0,m.jsx)(t.h1,{id:`border-tokens`,children:`Border Tokens`}),`
-`,(0,m.jsx)(t.h2,{id:`border-color`,children:`Border Color`}),`
-`,(0,m.jsx)(n,{of:s,style:{height:`auto`}}),`
-`,(0,m.jsx)(t.h2,{id:`border-size`,children:`Border Size`}),`
-`,(0,m.jsx)(n,{of:l,style:{height:`auto`}}),`
-`,(0,m.jsx)(t.h2,{id:`border-radius`,children:`Border Radius`}),`
-`,(0,m.jsx)(n,{of:c,style:{height:`auto`}})]})}function p(e={}){let{wrapper:t}={...i(),...e.components};return t?(0,m.jsx)(t,{...e,children:(0,m.jsx)(f,{...e})}):f(e)}var m;e((()=>{m=a(),o(),t(),u()}))();export{p as default};
--- assets/list-Ce2_FFFl.js
+++
@@ -1,4 +0,0 @@
-import{o as e}from"./preload-helper-CHxnduP2.js";import{f as t,n,s as r,w as i}from"./blocks-D7o89Cd2.js";import{s as a}from"./chunk-LITCR56V-CZnCZIqS.js";import{t as o}from"./mdx-react-shim-BelAooES.js";import{d as s,n as c}from"./ClassExample-B6uuMYsl.js";import{ListReset as l,n as u,t as d}from"./list.stories-CjopMbAw.js";function f(e){let t={h1:`h1`,p:`p`,...i(),...e.components};return(0,m.jsxs)(m.Fragment,{children:[(0,m.jsx)(r,{of:u}),`
-`,(0,m.jsx)(t.h1,{id:`list-reset`,children:`List reset`}),`
-`,(0,m.jsx)(t.p,{children:`Resets browser defaults on lists`}),`
-`,(0,m.jsx)(n,{of:l})]})}function p(e={}){let{wrapper:t}={...i(),...e.components};return t?(0,m.jsx)(t,{...e,children:(0,m.jsx)(f,{...e})}):f(e)}var m;e((()=>{m=a(),o(),t(),s(),c(),d()}))();export{p as default};

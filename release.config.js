bbb7b240b5785afe3b17bb834a11df204d32edde
Adam Detrick <adam.detrick@narmitech.com>
2026-07-31 14:24:10 -0400

    fix: include maint branches in release config

--- release.config.js
+++ release.config.js
@@ -5,7 +5,15 @@
  * `semantic-release` runs with the following configuration via the `release.yml` github action.
  */
 const config = {
-  branches: ["main"],
+  branches: [
+    {
+      name: "maintenance/+([0-9]).+([0-9]).x",
+      channel: "${name.replace(/^maintenance\\//g, '')}",
+      range: "${name.replace(/^maintenance\\//g, '')}",
+    },
+    "main",
+    "maintenance/+([0-9]).+([0-9]).x",
+  ],
   plugins: [
     [
       "@semantic-release/commit-analyzer",

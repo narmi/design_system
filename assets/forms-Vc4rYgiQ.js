2fb61a56dcf0a0374bd8d9ecdcaf35a7768f2c2e
wrbrand <will.brand@gmail.com>
2026-08-04 20:34:23 +0000

    Deploying to gh-pages from @ narmi/design_system@f31d2461b113fd8fcbbf4027ac457b9c00eb5622 🚀

---
+++ assets/forms-Vc4rYgiQ.js
@@ -0,0 +1,4 @@
+import{o as e}from"./preload-helper-CHxnduP2.js";import{f as t,n,s as r,w as i}from"./blocks-njlHxKM2.js";import{s as a}from"./chunk-LITCR56V-CdjXNrQE.js";import{t as o}from"./mdx-react-shim-BgM4SmV9.js";import{d as s,n as c}from"./ClassExample-UX2A73ck.js";import{ButtonReset as l,n as u,t as d}from"./forms.stories-D3broz3p.js";function f(e){let t={code:`code`,h1:`h1`,p:`p`,...i(),...e.components};return(0,m.jsxs)(m.Fragment,{children:[(0,m.jsx)(r,{of:d}),`
+`,(0,m.jsx)(t.h1,{id:`button-reset`,children:`Button reset`}),`
+`,(0,m.jsxs)(t.p,{children:[`Resets browser defaults on `,(0,m.jsx)(t.code,{children:`button`}),` element.`]}),`
+`,(0,m.jsx)(n,{of:l})]})}function p(e={}){let{wrapper:t}={...i(),...e.components};return t?(0,m.jsx)(t,{...e,children:(0,m.jsx)(f,{...e})}):f(e)}var m;e((()=>{m=a(),o(),t(),s(),c(),u()}))();export{p as default};

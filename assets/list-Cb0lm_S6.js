b95980720f654ae7305bb5980a8b1260bf4a471d
akdetrick <adam.detrick@narmitech.com>
2026-08-11 19:28:49 +0000

    Deploying to gh-pages from @ narmi/design_system@f3f7538f929dfc07d04da9b030de714fd555b8c0 🚀

---
+++ assets/list-Cb0lm_S6.js
@@ -0,0 +1,4 @@
+import{o as e}from"./preload-helper-CHxnduP2.js";import{f as t,n,s as r,w as i}from"./blocks-CvUFA4Up.js";import{s as a}from"./chunk-LITCR56V-C_Zrn-LF.js";import{t as o}from"./mdx-react-shim-BgwZy4IL.js";import{d as s,n as c}from"./ClassExample-9fcSZxf6.js";import{ListReset as l,n as u,t as d}from"./list.stories-CZYGFyFP.js";function f(e){let t={h1:`h1`,p:`p`,...i(),...e.components};return(0,m.jsxs)(m.Fragment,{children:[(0,m.jsx)(r,{of:u}),`
+`,(0,m.jsx)(t.h1,{id:`list-reset`,children:`List reset`}),`
+`,(0,m.jsx)(t.p,{children:`Resets browser defaults on lists`}),`
+`,(0,m.jsx)(n,{of:l})]})}function p(e={}){let{wrapper:t}={...i(),...e.components};return t?(0,m.jsx)(t,{...e,children:(0,m.jsx)(f,{...e})}):f(e)}var m;e((()=>{m=a(),o(),t(),s(),c(),d()}))();export{p as default};

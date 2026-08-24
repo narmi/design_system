d55cf97303b6041ddda8a99528eb324a7b48ffaa
Adam Detrick <adam.detrick@narmitech.com>
2026-08-24 13:49:11 -0400

    fix(Tooltip): remove zindex responsibility from hook

--- src/hooks/useDropdownLayer/index.ts
+++ src/hooks/useDropdownLayer/index.ts
@@ -204,9 +204,8 @@ const useDropdownLayer = ({
         ? anchorPositionStyles
         : polyFillLayerStyles),
 
-      // Always include display and z-index.
+      // Always include display
       display: isOpen ? "block" : "none",
-      zIndex: isPortalled ? 9 : 4,
     };
 
     return {

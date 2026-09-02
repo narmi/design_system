911cade2a7f92101ea6c9c0b3f5da08354058733
akdetrick <adam.detrick@narmitech.com>
2026-09-02 14:33:53 +0000

    Deploying to gh-pages from @ narmi/design_system@c62a29d8161a1730e62c42b7aa850c0a12783315 🚀

--- assets/list-DT9PMoTC.js
+++
@@ -1,4 +0,0 @@
-import{o as e}from"./preload-helper-CHxnduP2.js";import{f as t,n,s as r,w as i}from"./blocks-B0EtsADj.js";import{s as a}from"./chunk-LITCR56V-Dsnm9V32.js";import{t as o}from"./mdx-react-shim-C3zU1KfU.js";import{d as s,n as c}from"./ClassExample-C6PV9cK6.js";import{ListReset as l,n as u,t as d}from"./list.stories-B8kYDyib.js";function f(e){let t={h1:`h1`,p:`p`,...i(),...e.components};return(0,m.jsxs)(m.Fragment,{children:[(0,m.jsx)(r,{of:u}),`
-`,(0,m.jsx)(t.h1,{id:`list-reset`,children:`List reset`}),`
-`,(0,m.jsx)(t.p,{children:`Resets browser defaults on lists`}),`
-`,(0,m.jsx)(n,{of:l})]})}function p(e={}){let{wrapper:t}={...i(),...e.components};return t?(0,m.jsx)(t,{...e,children:(0,m.jsx)(f,{...e})}):f(e)}var m;e((()=>{m=a(),o(),t(),s(),c(),d()}))();export{p as default};

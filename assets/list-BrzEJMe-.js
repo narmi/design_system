0ac8c424da87e309828a27358c6b062cdff23282
wrbrand <will.brand@gmail.com>
2026-09-11 17:16:08 +0000

    Deploying to gh-pages from @ narmi/design_system@832b5ec9929f5a359722d206614b966183a06170 🚀

---
+++ assets/list-BrzEJMe-.js
@@ -0,0 +1,4 @@
+import{o as e}from"./preload-helper-CHxnduP2.js";import{f as t,n,s as r,w as i}from"./blocks-DyxPl6Er.js";import{s as a}from"./chunk-LITCR56V-CbNwETij.js";import{t as o}from"./mdx-react-shim-02Sl1YnZ.js";import{d as s,n as c}from"./ClassExample-BrggCVQ_.js";import{ListReset as l,n as u,t as d}from"./list.stories-nSif0R9d.js";function f(e){let t={h1:`h1`,p:`p`,...i(),...e.components};return(0,m.jsxs)(m.Fragment,{children:[(0,m.jsx)(r,{of:u}),`
+`,(0,m.jsx)(t.h1,{id:`list-reset`,children:`List reset`}),`
+`,(0,m.jsx)(t.p,{children:`Resets browser defaults on lists`}),`
+`,(0,m.jsx)(n,{of:l})]})}function p(e={}){let{wrapper:t}={...i(),...e.components};return t?(0,m.jsx)(t,{...e,children:(0,m.jsx)(f,{...e})}):f(e)}var m;e((()=>{m=a(),o(),t(),s(),c(),d()}))();export{p as default};
